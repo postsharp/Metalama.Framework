bad2cbd683dcc3a00b6c681f6d10162691836291
Petr Onderka <petro@postsharp.net>
2020-12-14 12:47:39 +0100

    Further improve locating reference assemblies

--- Caravela.Framework.Impl/CompileTime/ReferenceAssemblyLocator.cs
+++ Caravela.Framework.Impl/CompileTime/ReferenceAssemblyLocator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using static Caravela.Framework.Impl.CompileTime.PackageVersions;
 
@@ -10,21 +11,7 @@ namespace Caravela.Framework.Impl.CompileTime
 {
     public static class ReferenceAssemblyLocator
     {
-        public static IEnumerable<string> GetReferenceAssemblies()
-        {
-            string version = typeof( CompileTimeAssemblyBuilder ).Assembly.GetName().Version.ToString();
-            string tempProjectDirectory = Path.Combine( Path.GetTempPath(), "Caravela", version, "TempProject" );
-
-            string referenceAssemlyListFile = Path.Combine( tempProjectDirectory, "assemblies.txt" );
-
-            if ( File.Exists( referenceAssemlyListFile ) )
-            {
-                return File.ReadAllLines( referenceAssemlyListFile );
-            }
-
-            Directory.CreateDirectory( tempProjectDirectory );
-
-            string project = $@"
+        private static readonly string project = $@"
 <Project Sdk='Microsoft.NET.Sdk'>
   <PropertyGroup>
     <TargetFramework>netstandard2.0</TargetFramework>
@@ -33,14 +20,36 @@ namespace Caravela.Framework.Impl.CompileTime
     <PackageReference Include='Microsoft.CSharp' Version='{MicrosoftCSharpVersion}' />
     <PackageReference Include='Microsoft.CodeAnalysis.CSharp' Version='{MicrosoftCodeAnalysisCSharpVersion}' />
   </ItemGroup>
+  <Target Name='WriteReferenceAssemblies' DependsOnTargets='FindReferenceAssembliesForReferences'>
+    <WriteLinesToFile File='assemblies.txt' Overwrite='true' Lines='@(ReferencePathWithRefAssemblies)' />
+  </Target>
 </Project>";
+
+        public static IEnumerable<string> GetReferenceAssemblies()
+        {
+            string hash = ComputeHash( project );
+            string tempProjectDirectory = Path.Combine( Path.GetTempPath(), "Caravela", hash, "TempProject" );
+
+            string referenceAssemlyListFile = Path.Combine( tempProjectDirectory, "assemblies.txt" );
+
+            if ( File.Exists( referenceAssemlyListFile ) )
+            {
+                var referenceAssemblies = File.ReadAllLines( referenceAssemlyListFile );
+
+                if ( referenceAssemblies.All( File.Exists ) )
+                    return referenceAssemblies;
+            }
+
+            Directory.CreateDirectory( tempProjectDirectory );
+
             File.WriteAllText( Path.Combine( tempProjectDirectory, "TempProject.csproj" ), project );
 
-            var psi = new ProcessStartInfo( "dotnet", "build -t:rebuild -clp:ShowCommandLine" )
+            var psi = new ProcessStartInfo( "dotnet", "build -t:WriteReferenceAssemblies" )
             {
                 WorkingDirectory = tempProjectDirectory,
                 UseShellExecute = false,
-                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true
             };
             var process = Process.Start( psi );
 
@@ -53,68 +62,14 @@ namespace Caravela.Framework.Impl.CompileTime
             if ( process.ExitCode != 0 )
                 throw new InvalidOperationException( "Error while building temporary project to locate reference assemblies:" + Environment.NewLine + string.Join( Environment.NewLine, lines ) );
 
-            var references = ReadReferencesFromBuildOutput( lines ).ToArray();
-
-            File.WriteAllLines( referenceAssemlyListFile, references );
-
-            return references;
+            return File.ReadAllLines( referenceAssemlyListFile );
         }
 
-        // Buildalyzer or MSBuild.StructuredLogger could be used to get this information from binlog instead, but they are quite heavy for this task
-        internal static IEnumerable<string> ReadReferencesFromBuildOutput( List<string> buildOutput )
+        private static string ComputeHash(string input)
         {
-            foreach ( var line in buildOutput )
-            {
-                if ( !line.Contains( "csc.dll" ) )
-                    continue;
-
-                foreach ( var argument in ParseArguments( line ) )
-                {
-                    string referencePrefix = "/reference:";
-                    if ( !argument.StartsWith( referencePrefix, StringComparison.Ordinal ) )
-                        continue;
-
-                    yield return argument.Substring( referencePrefix.Length );
-                }
-
-                yield break;
-            }
-
-            throw new InvalidOperationException( "Did not find csc command line in build output when building temporary project to locate reference assemblies" );
-        }
-
-        // https://stackoverflow.com/a/7774211/41071
-        internal static IEnumerable<string> ParseArguments( string commandLine )
-        {
-            if ( string.IsNullOrWhiteSpace( commandLine ) )
-                yield break;
-
-            var sb = new StringBuilder();
-            bool inQuote = false;
-            foreach ( char c in commandLine )
-            {
-                if ( c == '"' && !inQuote )
-                {
-                    inQuote = true;
-                    continue;
-                }
-
-                if ( c != '"' && !(char.IsWhiteSpace( c ) && !inQuote) )
-                {
-                    sb.Append( c );
-                    continue;
-                }
-
-                if ( sb.Length > 0 )
-                {
-                    yield return sb.ToString();
-                    sb.Clear();
-                    inQuote = false;
-                }
-            }
-
-            if ( sb.Length > 0 )
-                yield return sb.ToString();
+            using var sha1 = SHA1.Create();
+            var hash = sha1.ComputeHash( Encoding.UTF8.GetBytes( input ) );
+            return BitConverter.ToString( hash ).Replace( "-", "" );
         }
     }
 }
