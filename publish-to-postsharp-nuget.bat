6cf7063e5616c0d722a9cde5e15bf7c516285422
Petr Onderka <petro@postsharp.net>
2021-01-13 10:48:20 +0100

    Expanded the publishing script

--- publish-to-postsharp-nuget.bat
+++ publish-to-postsharp-nuget.bat
@@ -1,5 +1,13 @@
 @echo off
 
+IF "%~1" == "" GOTO Error
+IF "%~2" == "" GOTO Error
+
+set "CARAVELA_VERSION=%~1"
+set "NUGET_KEY=%~2"
+
+dotnet pack -c Release
+
 echo Publishing Caravela version %CARAVELA_VERSION%
 
 cd artifacts\bin\Release
@@ -10,3 +18,9 @@ dotnet nuget push -s https://nuget.postsharp.net/nuget/caravela/ -k %NUGET_KEY% 
 dotnet nuget push -s https://nuget.postsharp.net/nuget/caravela/ -k %NUGET_KEY% Caravela.Framework.Impl.%CARAVELA_VERSION%.nupkg 
 
 cd ..\..\..
+
+goto :eof
+
+:Error
+echo Command line incorrect, usage: %0 version nuget_key, e.g. %0 1.0 ABCDEFGHIJKLMNOPQRST
+echo Make sure to also update the version in Directory.Build.props and that you are connected to the PostSharp VPN
