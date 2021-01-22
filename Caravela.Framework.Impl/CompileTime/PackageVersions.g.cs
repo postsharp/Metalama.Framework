d079f454c66e1bd7882980b925265d6a203267a3
Petr Onderka <petro@postsharp.net>
2021-01-22 13:10:07 +0100

    Forgotten file

--- Caravela.Framework.Impl/CompileTime/PackageVersions.g.cs
+++ Caravela.Framework.Impl/CompileTime/PackageVersions.g.cs
@@ -2,8 +2,8 @@ namespace Caravela.Framework.Impl.CompileTime
 {
     static class PackageVersions
     {
-        public const string CaravelaCompilerVersion = "3.8.100";
-        public const string CaravelaCompilerSdkVersion = "3.8.100";
+        public const string CaravelaCompilerVersion = "3.8.0-preview";
+        public const string CaravelaCompilerSdkVersion = "3.8.0-preview";
         public const string MicrosoftCodeAnalysisCSharpVersion = "3.8.0";
         public const string MicrosoftCSharpVersion = "4.7.0";
     }
