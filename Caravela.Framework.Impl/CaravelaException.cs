d21f1d6ef1b32234c49909d1bb8f39168a7c0bc9
Petr Onderka <petro@postsharp.net>
2021-02-01 17:31:45 +0100

    Report exception with stack trace

--- Caravela.Framework.Impl/CaravelaException.cs
+++ Caravela.Framework.Impl/CaravelaException.cs
@@ -20,7 +20,6 @@ namespace Caravela.Framework.Impl
 
     static class ExceptionExtensions
     {
-        public static string ToDiagnosticString( this Exception ex ) =>
-            ex.InnerException == null ? $"{ex.GetType()}: {ex.Message}" : $"{ex.GetType()}: {ex.Message} -> {ex.InnerException.ToDiagnosticString()}";
+        public static string ToDiagnosticString( this Exception ex ) => ex.ToString();
     }
 }
