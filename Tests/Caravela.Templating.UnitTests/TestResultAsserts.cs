b81ab3d3bc72dd531958a594d117a58ab230b1a4
Petr Onderka <petro@postsharp.net>
2021-02-10 13:04:45 +0100

    Fix after merge

--- Tests/Caravela.Templating.UnitTests/TestResultAsserts.cs
+++ Tests/Caravela.Templating.UnitTests/TestResultAsserts.cs
@@ -10,8 +10,8 @@ namespace Caravela.Templating.UnitTests
     {
         public static void AssertOutput( this TestResult testResult, string expectedOutput )
         {
-            Assert.Null( testResult.TestErrorMessage );
-            Assert.Null( testResult.TestException );
+            Assert.Null( testResult.ErrorMessage );
+            Assert.Null( testResult.Exception );
 
             // Don't test output if we have an error.
             if ( testResult.Diagnostics.Any( d => d.Severity == DiagnosticSeverity.Error ) )
