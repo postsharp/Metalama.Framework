2358ec96f449b1e901f361b51e43847ccd8caf93
Gael Fraiteur <gael@postsharp.net>
2021-02-14 22:23:35 +0100

    Code formatting, solving warnings, clean up.

---
+++ Caravela.Framework.Impl/AspectPartId.cs
@@ -0,0 +1,11 @@
+﻿using Caravela.Framework.Code;
+
+namespace Caravela.Framework.Impl
+{
+    internal record AspectPartId( string AspectType, string? PartName )
+    {
+        public AspectPartId( INamedType aspectType, string? partName ) : this( aspectType.FullName, partName )
+        {
+        }
+    }
+}
