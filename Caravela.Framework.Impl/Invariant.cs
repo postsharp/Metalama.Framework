b98b58ac0ed484fa4ceb09cc814cafbf182e4d0f
Gael Fraiteur <gael@postsharp.net>
2021-02-18 14:39:02 +0100

    Changing Debug.Assert to Invariant.Assert.

---
+++ Caravela.Framework.Impl/Invariant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Caravela.Framework.Impl
+{
+    /// <summary>
+    /// A utility class that checks runtime invariant and throws <see cref="AssertionFailedException"/> in case of failure.
+    /// </summary>
+    internal static class Invariant
+    {
+
+        /// <summary>
+        /// Checks that a given condition is true and throws an <see cref="AssertionFailedException"/> in case it is not.
+        /// </summary>
+        /// <param name="condition">The condition that must be true.</param>
+        /// <param name="description">An error that is typically constructed as "X must be Y".</param>
+        public static void Assert( [DoesNotReturnIf( false )] bool condition, string description )
+        {
+            if ( !condition )
+            {
+                throw new AssertionFailedException( "Assertion failed: " + description + "." );
+            }
+        }
+
+
+        /// <summary>
+        /// Checks that a reference is non-null and throws an <see cref="AssertionFailedException"/> if it is not.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T AssertNotNull<T>( this T? obj )
+            where T : class
+        {
+            if ( obj == null )
+            {
+                Assert( false, $"the reference to {typeof( T ).Name} must no be not null" );
+            }
+            
+            return obj;
+        }
+    }
+}
