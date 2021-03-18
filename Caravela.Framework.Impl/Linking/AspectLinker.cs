3232727d6bd16937a972c2ecb1d4d39e57ad0347
Daniel Balas <daniel@postsharp.net>
2021-03-18 11:06:19 +0100

    #28310 Fixed code warnings.

--- Caravela.Framework.Impl/Linking/AspectLinker.cs
+++ Caravela.Framework.Impl/Linking/AspectLinker.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
-using System;
-using Microsoft.CodeAnalysis;
-
 namespace Caravela.Framework.Impl.Linking
 {
     internal partial class AspectLinker
