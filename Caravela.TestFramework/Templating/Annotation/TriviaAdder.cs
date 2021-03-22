0653474ebca66b366d07a0bbba184ccce591d8e6
Antonín Procházka <antonin@postsharp.net>
2021-03-22 12:41:15 +0100

    #28324 Refactoring.

--- Caravela.TestFramework/Templating/Annotation/TriviaAdder.cs
+++ Caravela.TestFramework/Templating/Annotation/TriviaAdder.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
-namespace Caravela.TestFramework
+namespace Caravela.TestFramework.Templating.Annotation
 {
+    /// <summary>
+    /// Adds a space before and after each node.
+    /// </summary>
     public class TriviaAdder : CSharpSyntaxRewriter
     {
         public override SyntaxNode? Visit( SyntaxNode? node )
