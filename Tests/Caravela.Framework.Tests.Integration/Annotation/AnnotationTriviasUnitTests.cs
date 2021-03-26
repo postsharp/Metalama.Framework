0ac7f199033efe52b00430b32c519d355c6a9015
Gael Fraiteur <gael@postsharp.net>
2021-03-26 15:13:59 +0100

    Delete redundant tests.

--- Tests/Caravela.Framework.Tests.Integration/Annotation/AnnotationTriviasUnitTests.cs
+++ Tests/Caravela.Framework.Tests.Integration/Annotation/AnnotationTriviasUnitTests.cs
@@ -15,39 +15,8 @@ namespace Caravela.Framework.Tests.Integration.Annotation
         }
 
         [Theory]
-        [FromDirectory( @"TestInputs\Formatting\Declarations" )]
-        public Task Declarations( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
+        [FromDirectory( @"TestInputs\Formatting" )]
+        public Task All( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
 
-        [Theory]
-        [FromDirectory( @"TestInputs\Formatting\Identifiers" )]
-        public Task Identifiers( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
-
-        [Theory]
-        [FromDirectory( @"TestInputs\Formatting\MemberAccess" )]
-        public Task MemberAccess( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
-
-        [Theory]
-        [FromDirectory( @"TestInputs\Formatting\InvocationExpressions" )]
-        public Task InvocationExpressions( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
-
-        [Theory]
-        [FromDirectory( @"TestInputs\Formatting\IfStatements" )]
-        public Task IfStatements( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
-
-        [Theory]
-        [FromDirectory( @"TestInputs\Formatting\ForEachStatements" )]
-        public Task ForEachStatements( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
-
-        [Theory]
-        [FromDirectory( @"TestInputs\Formatting\ForStatements" )]
-        public Task ForStatements( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
-
-        [Theory( Skip = "Not supported yet." )]
-        [FromDirectory( @"TestInputs\Formatting\WhileStatements" )]
-        public Task WhileStatements( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
-
-        [Theory( Skip = "Not supported yet." )]
-        [FromDirectory( @"TestInputs\Formatting\DoWhileStatements" )]
-        public Task DoWhileStatements( string testName ) => this.AssertTriviasPreservedByAnnotator( testName );
     }
 }
