3d5019c7d9f83501ccba0857d534ceee96ecd1fe
Gael Fraiteur <gael@postsharp.net>
2021-03-31 17:56:08 +0200

    Fixed previous commit.

--- Caravela.Framework/Aspects/AspectLinkerOptionsAttribute.cs
+++ Caravela.Framework/Aspects/AspectLinkerOptionsAttribute.cs
@@ -2,14 +2,14 @@
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
 using System;
+using System.ComponentModel;
 
 namespace Caravela.Framework.Aspects
 {
-    /// <summary>
-    /// Specifies behavior of the aspect linker on the declaration (for testing only).
-    /// </summary>
+    /// <exclude />
     [AttributeUsage( AttributeTargets.All, AllowMultiple = true )]
-    internal sealed class AspectLinkerOptionsAttribute : Attribute
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public sealed class AspectLinkerOptionsAttribute : Attribute
     {
         /// <summary>
         /// Gets or sets a value indicating whether the linker inlining of this member is forcefully disabled, even if it would be otherwise possible.
