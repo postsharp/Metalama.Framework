64c3706a7440a4755b7a1e864a5646c35296edf3
Antonín Procházka <antonin@postsharp.net>
2021-05-25 14:48:15 +0200

    #28588 InitializeRepo.ps1 improved.

--- .engineering/InitializeRepo.ps1
+++ .engineering/InitializeRepo.ps1
@@ -1,4 +1,15 @@
-git subtree add --prefix .engineering https://postsharp@dev.azure.com/postsharp/Caravela/_git/Caravela.Engineering master --squash
-New-Item -Path ..\.editorconfig -ItemType SymbolicLink -Value .\.editorconfig
-New-Item -Path ..\Directory.Build.props -ItemType SymbolicLink -Value .\Directory.Build.props
-New-Item -Path ..\Directory.Build.targets -ItemType SymbolicLink -Value .\Directory.Build.targets
+$ErrorActionPreference = "Stop"
+
+If ( -Not ( Test-Path -Path ".\.git" ) ) {
+    throw "This script has to run in a GIT repository root!"
+}
+
+$EngineeringDirectory = ".engineering"
+
+git subtree add --prefix $EngineeringDirectory https://postsharp@dev.azure.com/postsharp/Caravela/_git/Caravela.Engineering master --squash
+
+
+Get-ChildItem ".\$EngineeringDirectory\linked\root\" | 
+Foreach-Object {
+    New-Item -Path ".\$($_.Name)" -ItemType SymbolicLink -Value $_.FullName
+}
