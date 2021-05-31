77bf3fb815c07b0baa0cf02bf296daec7e518b3f
Gael Fraiteur <gael@postsharp.net>
2021-05-31 11:47:02 +0200

    Documentation.

--- Caravela.Framework/Aspects/IAspectClassBuilder.cs
+++ Caravela.Framework/Aspects/IAspectClassBuilder.cs
@@ -6,15 +6,31 @@ using System.Collections.Immutable;
 
 namespace Caravela.Framework.Aspects
 {
+    /// <summary>
+    /// An object used by implementations of the <see cref="IAspect.BuildAspectClass"/> method to set the static characteristics of
+    /// the aspect class, i.e. those that do not depend on the aspect instance state.
+    /// </summary>
     [InternalImplement]
     public interface IAspectClassBuilder
     {
+        /// <summary>
+        /// Gets or sets the name of the aspect as shown to the user at design time.
+        /// </summary>
         string DisplayName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the description of the aspect.
+        /// </summary>
         string? Description { get; set; }
 
+        /// <summary>
+        /// Gets or sets the list of layers of the aspect. Layers must be sorted by order of execution (e.g. inverse order of application).
+        /// </summary>
         ImmutableArray<string> Layers { get; set; }
 
+        /// <summary>
+        /// Gets a service allowing to specify the dependencies of the current aspect class.
+        /// </summary>
         IAspectDependencyBuilder Dependencies { get; }
     }
 }
