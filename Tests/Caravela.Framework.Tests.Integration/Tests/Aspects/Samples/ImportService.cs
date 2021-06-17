84e96e70dad30af1ef8ec5f51d0952bf78836adb
Daniel Balas <daniel@postsharp.net>
2021-06-17 17:42:30 +0200

    #28774 Structural symbol comparer fields.

---
+++ Tests/Caravela.Framework.Tests.Integration/Tests/Aspects/Samples/ImportService.cs
@@ -0,0 +1,39 @@
+﻿using Caravela.Framework.Aspects;
+using Caravela.TestFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caravela.Framework.Tests.Integration.Tests.Aspects.Samples.ImportService
+{
+    class ImportServiceAspect : OverrideFieldOrPropertyAspect
+    {
+
+        public override dynamic OverrideProperty
+        {
+            get
+            {
+                return meta.This._serviceProvider.GetService(meta.FieldOrProperty.Type.ToType());
+            }
+
+            set
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+
+    [TestOutput]
+    class TargetClass
+    {
+        private readonly IServiceProvider? _serviceProvider;
+
+        [ImportServiceAspect]
+        private IFormatProvider? FormatProvider { get; }
+
+        public string? Format(object? o)
+        {
+            return ((ICustomFormatter)this.FormatProvider.GetFormat(typeof(ICustomFormatter)))?.Format(null, o, this.FormatProvider);
+        }
+    }
+}
---
+++ Tests/Caravela.Framework.Tests.Integration/Tests/Aspects/Samples/ImportService.t.cs
@@ -0,0 +1,19 @@
+[TestOutput]
+class TargetClass
+{
+    private readonly IServiceProvider? _serviceProvider;
+    [ImportServiceAspect]
+    private IFormatProvider? FormatProvider
+    {
+        get
+        {
+            return (System.IFormatProvider?)this._serviceProvider.GetService(global::System.Type.GetTypeFromHandle(global::Caravela.Compiler.Intrinsics.GetRuntimeTypeHandle("T:System.IFormatProvider")));
+        }
+    }
+
+    private readonly IFormatProvider? __FormatProvider__BackingField;
+    public string? Format(object? o)
+    {
+        return ((ICustomFormatter)this.FormatProvider.GetFormat(typeof(ICustomFormatter)))?.Format(null, o, this.FormatProvider);
+    }
+}
