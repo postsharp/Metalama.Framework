ed881ae5a562aa83f8ff5848e87a5384c9d92e74
Antonín Procházka <antonin@postsharp.net>
2021-07-08 11:39:24 +0200

    #28588 Engineering fixed.

--- .engineering/PullEngineering.ps1
+++ .engineering/PullEngineering.ps1
@@ -12,22 +12,27 @@ If ( -Not ( Test-Path -Path ".\.git" ) ) {
 # Update/initialize the engineering subtree.
 $EngineeringDirectory = ".engineering"
 
-If ( ( Test-Path -Path $EngineeringDirectory ) ) {
+If ( Test-Path -Path $EngineeringDirectory ) {
     $SubtreeCommand = "pull"
 } else {
     $SubtreeCommand = "add"
 }
 
-& git subtree $SubtreeCommand --prefix $EngineeringDirectory https://postsharp@dev.azure.com/postsharp/Caravela/_git/Caravela.Engineering master --squash
+# & git subtree $SubtreeCommand --prefix $EngineeringDirectory https://postsharp@dev.azure.com/postsharp/Caravela/_git/Caravela.Engineering master --squash
 
-If ( $LastExitCode -Ne 0 ) {
-    throw "Failed to $SubtreeCommand subtree."
-}
+# If ( $LastExitCode -Ne 0 ) {
+#    throw "Failed to $SubtreeCommand subtree."
+# }
 
 # Clean-up existing links or copies of linked files. This also solves issues with invalid merges or updates,
 # where a link file is replaced by a file containing the original target path.
 # Keep this clean-up part up to date, so that this script can be executed on any repository in any state.
-Remove-Item ".\.editorconfig"
+
+$EditorConfigFile = ".\.editorconfig"
+
+if ( Test-Path -Path $EditorConfigFile ) {
+    Remove-Item $EditorConfigFile
+}
 
 Get-ChildItem ".\" -Filter "*.sln.DotSettings" | 
 Foreach-Object {
@@ -36,11 +41,13 @@ Foreach-Object {
 
 # Link files.
 # If the creation of the symlinks fails, either the script needs to be executed with elevation, or the Windows Developer Mode needs to be enabled.
-New-Item -Path ".\.editorconfig" -ItemType SymbolicLink -Value ".\$EngineeringDirectory\style\.editorconfig"
+# We have to use the mklink command instead of the New-Command cmd-let, because the New-Command cmd-let requires elevation even with the Windows Developer Mode enabled.
+# We have to execute the mklink command in a Windows Command Prompt process, because mklink is not a stand-alone executable, but a built-in command.
+& cmd /c mklink $EditorConfigFile ".\$EngineeringDirectory\style\.editorconfig"
 
 Get-ChildItem ".\" -Filter "*.sln" | 
 Foreach-Object {
-    New-Item -Path "$($_.FullName).DotSettings" -ItemType SymbolicLink -Value ".\$EngineeringDirectory\style\sln.DotSettings"
+    & cmd /c mklink "$($_.FullName).DotSettings" ".\$EngineeringDirectory\style\sln.DotSettings"
 }
 
 # Remove the executed copy of this script.
