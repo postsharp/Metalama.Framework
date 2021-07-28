22a9dd901308223d9811a74e8240eb9c3c5bf94f
Gael Fraiteur <gael@postsharp.net>
2021-07-28 09:16:32 +0200

    Fixing warnings.

--- Tests/Caravela.Framework.Tests.PublicPipeline/Tests/Aspects/Bugs/Bug28973.t.cs
+++ Tests/Caravela.Framework.Tests.PublicPipeline/Tests/Aspects/Bugs/Bug28973.t.cs
@@ -1,9 +1,10 @@
-// Warning CS8601 on `service`: `Possible null reference assignment.`
 class TargetCode
     {
         [Import]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         IFormatProvider FormatProvider {get    {
         return (System.IFormatProvider)global::Caravela.Framework.Tests.Integration.Aspects.Bugs.Bug28973.ServiceLocator.ServiceProvider.GetService(typeof(global::System.IFormatProvider));
     }
 }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     }
