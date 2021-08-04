8bd9e975f137ff7259dcb25816b640952ec5d22c
Gael Fraiteur <gael@postsharp.net>
2021-08-04 15:36:42 +0200

    Formatting.

--- Caravela.Framework/Aspects/IMetaApi.cs
+++ Caravela.Framework/Aspects/IMetaApi.cs
@@ -37,7 +37,6 @@ namespace Caravela.Framework.Aspects
 
         dynamic BaseStatic { get; }
 
-        // Gets the properties that were passed by the aspect initializer.
         IReadOnlyDictionary<string, object?> Tags { get; }
 
         IDiagnosticSink Diagnostics { get; }
