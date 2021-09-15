53571a3f393c7e97c45c0872dcf8a32fa84df5cd
Gael Fraiteur <gael@postsharp.net>
2021-09-15 21:50:33 +0200

    Fixed test output broken by mistake.

--- Tests/Caravela.Framework.Tests.InternalPipeline/Tests/Templating/Dynamic/ArrayBuilder.t.cs
+++ Tests/Caravela.Framework.Tests.InternalPipeline/Tests/Templating/Dynamic/ArrayBuilder.t.cs
@@ -1,6 +1,6 @@
 private int Method( int a, string c, DateTime dt )
 {
-    var array1 = global::Caravela.Framework.Caravela.Framework.Code.ExpressionBuilders.ExpressionBuilderExtensions.ToValue(new global::System.Object[]{a, c, dt});
+    var array1 = new global::System.Object[]{a, c, dt};
     var array2 = new global::System.Object[]{a, c, dt};
     return default;
 }
