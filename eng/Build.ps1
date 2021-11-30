ff8c5d9a62093c43be879a4c234406bd72577fab
Antonín Procházka <antonin@postsharp.net>
2021-11-30 15:33:37 +0100

    Upgraded to Roslyn 4.0, .NET 6 SDK and VS 2022.

--- eng/Build.ps1
+++ eng/Build.ps1
@@ -25,8 +25,8 @@ param (
 )
 
 if ( $env:VisualStudioVersion -eq $null ) {
-    Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-    Enter-VsDevShell -VsInstallPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\" -StartInPath $(Get-Location)
+    Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+    Enter-VsDevShell -VsInstallPath "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\" -StartInPath $(Get-Location)
 }
 
 # Map the old arguments to the new one
