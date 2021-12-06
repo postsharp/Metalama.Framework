45a5bf2a240074dc549099d07854aca498f77c59
Daniel Balas <daniel@postsharp.net>
2021-12-06 14:58:27 +0100

    #29392 Missing files.

---
+++ Tests/Caravela.Framework.Tests.UnitTests.Internals/CompileTime/Serializers/InheritanceTests.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
+// This project is not open source. Please see the LICENSE.md file in the repository root for details.
+
+using Caravela.TestFramework;
+using System;
+using Xunit;
+
+namespace Caravela.Framework.Tests.UnitTests.CompileTime.Serializers
+{
+    public class InheritanceTests : MetaSerializerTestBase
+    {
+
+        [Fact]
+        public void BaseSerializerInTheSameAssembly()
+        {
+            // Verifies that IMetaSerializable compile-time type with explicit parameterless constructor can be serialized and deserialized.
+            // Generator should not inject parameterless constructor when it is already defined.
+            var code = @"
+using Caravela.Framework.Aspects;
+using Caravela.Framework.Serialization;
+[assembly: CompileTime]
+public class A : IMetaSerializable
+{
+    public int BaseField;
+}
+public class B : A
+{
+    public int DerivedField;
+}
+";
+
+            using var domain = new UnloadableCompileTimeDomain();
+            using var testContext = this.CreateTestContext();
+
+/* Unmerged change from project 'Caravela.Framework.Tests.UnitTests.Internals (netframework4.8)'
+Before:
+            var project = this.CreateCompileTimeProject( domain, testContext, code );
+After:
+            var project = MetaSerializerTestBase.CreateCompileTimeProject( domain, testContext, code );
+*/
+            var project = CreateCompileTimeProject( domain, testContext, code );
+
+            var type = project!.GetType( "B" );
+            var metaSerializer = GetMetaSerializer( type );
+
+            dynamic instance = Activator.CreateInstance( type )!;
+            instance.BaseField = 13;
+            instance.DerivedField = 42;
+
+            var constructorArgumentsWriter = new TestArgumentsWriter();
+            var initializationArgumentsWriter = new TestArgumentsWriter();
+            metaSerializer.SerializeObject( instance, constructorArgumentsWriter, initializationArgumentsWriter );
+
+            var constructorArgumentsReader = constructorArgumentsWriter.ToReader();
+            var initializationArgumentsReader = initializationArgumentsWriter.ToReader();
+
+            dynamic deserialized = metaSerializer.CreateInstance( type, constructorArgumentsReader );
+            metaSerializer.DeserializeFields( ref deserialized, initializationArgumentsReader );
+
+            Assert.Equal( 13, deserialized.BaseField );
+            Assert.Equal( 42, deserialized.DerivedField );
+        }
+    }
+}
---
+++ Tests/Caravela.Framework.Tests.UnitTests.Internals/CompileTime/Serializers/MetaSerializerTestBase.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
+// This project is not open source. Please see the LICENSE.md file in the repository root for details.
+
+using Caravela.Framework.Impl;
+using Caravela.Framework.Impl.CompileTime;
+using Caravela.Framework.Impl.Diagnostics;
+using Caravela.Framework.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Caravela.Framework.Tests.UnitTests.CompileTime.Serializers
+{
+    public class MetaSerializerTestBase : TestBase
+    {
+        private protected static CompileTimeProject CreateCompileTimeProject( CompileTimeDomain domain, TestContext testContext, string code )
+        {
+            var runtimeCompilation = CreateCSharpCompilation(
+                code,
+                name: "test_A" );
+
+            var compileTimeCompilationBuilder = new CompileTimeCompilationBuilder( testContext.ServiceProvider, domain );
+            DiagnosticList diagnosticList = new();
+
+            Assert.True(
+                compileTimeCompilationBuilder.TryGetCompileTimeProject( runtimeCompilation, null, Array.Empty<CompileTimeProject>(), diagnosticList, false, CancellationToken.None, out var project ),
+                string.Join( "\n", diagnosticList.Select( x => x.ToString() ) ) );
+
+            return project!;
+        }
+
+        private protected static Type GetMetaSerializerType( Type type )
+        {
+            var metaSerializerTypes =
+               type.GetNestedTypes()
+               .Where( nestedType => typeof( IMetaSerializer ).IsAssignableFrom( nestedType ) )
+               .ToArray();
+
+            Assert.Single( metaSerializerTypes );
+
+            return metaSerializerTypes[0];
+        }
+
+        private protected static IMetaSerializer GetMetaSerializer( Type type )
+        {
+            var metaSerializerType = GetMetaSerializerType( type );
+            return (IMetaSerializer) Activator.CreateInstance( metaSerializerType ).AssertNotNull();
+        }
+
+        private protected class TestArgumentsReader : IArgumentsReader
+        {
+            private (string Name, object? Value, string? Scope)[]? _data;
+
+            public virtual bool TryGetValue<T>( string name, [NotNullWhen( true )] out T value, string? scope = null )
+            {
+                var dataValue =
+                    this._data.AssertNotNull()
+                    .Select( x => ((string Name, object? Value, string? Scope)?) x )
+                    .SingleOrDefault( d => StringComparer.Ordinal.Equals( d.AssertNotNull().Name, name ) && StringComparer.Ordinal.Equals( d.AssertNotNull().Scope, scope ) );
+
+                if ( dataValue == null )
+                {
+                    value = default!;
+                    return false;
+                }
+
+                value = (T) dataValue.Value.Value!;
+                return true;
+            }
+
+            public virtual T GetValue<T>( string name, string? scope = null )
+            {
+                if ( !this.TryGetValue<T>( name, out var value, scope ) )
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return value;
+            }
+
+            public void SetData( params (string Name, object? Value, string? Scope)[] data )
+            {
+                this._data = data;
+            }
+        }
+
+        private protected class ThrowingArgumentsWriter : IArgumentsWriter
+        {
+            public virtual void SetValue( string name, object? value, string? scope = null )
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        private protected class TestArgumentsWriter : IArgumentsWriter
+        {
+            private List<(string Name, object? Value, string? Scope)> Data { get; } = new List<(string Name, object? Value, string? Scope)>();
+
+            public virtual void SetValue( string name, object? value, string? scope = null )
+            {
+                this.Data.Add( (name, value, scope) );
+            }
+
+            public virtual TestArgumentsReader ToReader()
+            {
+                var reader = new TestArgumentsReader();
+                reader.SetData( this.Data.ToArray() );
+                return reader;
+            }
+        }
+    }
+}
