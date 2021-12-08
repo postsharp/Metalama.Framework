1534c3e51f269bb71b10f6a092199085e8d2f38b
Gael Fraiteur <gael@postsharp.net>
2021-12-08 14:42:48 +0100

    Automatic generation of the syntax file.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -2,10 +2,14 @@
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
+using PostSharp.Engineering.BuildTools.Utilities;
 using Spectre.Console.Cli;
+using System;
 using System.Collections.Immutable;
+using System.IO;
 
 var product = new Product
 {
@@ -24,20 +28,57 @@ var product = new Product
             IsTestOnly = true
         } ),
     PublicArtifacts = Pattern.Create(
-        "Caravela.Framework.$(PackageVersion).nupkg", 
+        "Caravela.Framework.$(PackageVersion).nupkg",
         "Caravela.TestFramework.$(PackageVersion).nupkg",
         "Caravela.Framework.Redist.$(PackageVersion).nupkg",
-        "Caravela.Framework.Sdk.$(PackageVersion).nupkg", 
+        "Caravela.Framework.Sdk.$(PackageVersion).nupkg",
         "Caravela.Framework.Impl.$(PackageVersion).nupkg",
         "Caravela.Framework.DesignTime.Contracts.$(PackageVersion).nupkg" ),
     Dependencies = ImmutableArray.Create(
         Dependencies.PostSharpEngineering,
         Dependencies.CaravelaCompiler,
-		Dependencies.PostSharpBackstageSettings )
+        Dependencies.PostSharpBackstageSettings )
 };
+
+product.PrepareCompleted += OnPrepareCompleted;
 
 var commandApp = new CommandApp();
 
 commandApp.AddProductCommands( product );
 
 return commandApp.Run( args );
+
+static bool OnPrepareCompleted( (BuildContext Context, BaseBuildSettings Settings) arg )
+{
+    arg.Context.Console.WriteHeading( "Generating code" );
+
+    var generatorDirectory = Path.Combine( arg.Context.RepoDirectory, "Build", "Caravela.Framework.GenerateMetaSyntaxRewriter" );
+    var project = new DotNetSolution( Path.Combine( generatorDirectory, "Caravela.Framework.GenerateMetaSyntaxRewriter.csproj" ) );
+
+    if ( !project.Restore( arg.Context, new BuildSettings() ) )
+    {
+        return false;
+    }
+
+    if ( !project.Build( arg.Context, new BuildSettings() ) )
+    {
+        return false;
+    }
+
+    var toolDirectory = Path.Combine( generatorDirectory, "bin", "Debug", "net48" );
+    var toolPath = Path.Combine( toolDirectory, "Caravela.Framework.GenerateMetaSyntaxRewriter.exe" );
+    if ( !ToolInvocationHelper.InvokeTool( arg.Context.Console, toolPath, "", toolDirectory ) )
+    {
+        return false;
+    }
+
+    var targetFile = Path.Combine( arg.Context.RepoDirectory, "Caravela.Framework.Impl", "Templating", "MetaSyntaxRewriter.g.cs" );
+    if ( File.Exists( targetFile ) )
+    {
+        File.Delete( targetFile );
+    }
+
+    File.Copy( Path.Combine( toolDirectory, "MetaSyntaxRewriter.g.cs" ), targetFile );
+
+    return true;
+}
