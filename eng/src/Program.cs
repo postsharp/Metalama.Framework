319f4e68036befa1b764242a706ba8f4affcb78b
Gael Fraiteur <gael@postsharp.net>
2021-12-08 11:09:00 +0100

    Updated engineering.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -7,55 +7,36 @@ using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
 using System.Collections.Immutable;
 
-namespace BuildCaravela
+var product = new Product
 {
-    internal static class Program
-    {
-        private static int Main( string[] args )
+    ProductName = "Caravela",
+    Solutions = ImmutableArray.Create<Solution>(
+        new DotNetSolution( "Caravela.sln" )
         {
-            var privateSource = new NugetSource( "%INTERNAL_NUGET_PUSH_URL%", "%INTERNAL_NUGET_API_KEY%" );
-            var publicSource = new NugetSource( "https://api.nuget.org/v3/index.json", "%NUGET_ORG_API_KEY%" );
+            SupportsTestCoverage = true,
+            CanFormatCode = true,
+            FormatExclusions = ImmutableArray.Create(
+                "Tests\\Caravela.Framework.Tests.Integration\\Tests\\**\\*",
+                "Tests\\Caravela.Framework.Tests.Integration.Internals\\Tests\\**\\*" )
+        },
+        new DotNetSolution( "Tests\\Caravela.Framework.TestApp\\Caravela.Framework.TestApp.sln" )
+        {
+            IsTestOnly = true
+        } ),
+    PublicArtifacts = Pattern.Create(
+        "Caravela.Framework.$(PackageVersion).nupkg", 
+        "Caravela.TestFramework.$(PackageVersion).nupkg",
+        "Caravela.Framework.Redist.$(PackageVersion).nupkg",
+        "Caravela.Framework.Sdk.$(PackageVersion).nupkg", 
+        "Caravela.Framework.Impl.$(PackageVersion).nupkg",
+        "Caravela.Framework.DesignTime.Contracts.$(PackageVersion).nupkg" ),
+    Dependencies = ImmutableArray.Create(
+        Dependencies.PostSharpEngineering,
+        Dependencies.CaravelaCompiler )
+};
 
-            // These packages are published to internal and private feeds.
-            var publicPackages = new ParametricString[]
-            {
-                "Caravela.Framework.$(PackageVersion).nupkg", "Caravela.TestFramework.$(PackageVersion).nupkg",
-                "Caravela.Framework.Redist.$(PackageVersion).nupkg",
-                "Caravela.Framework.Sdk.$(PackageVersion).nupkg", "Caravela.Framework.Impl.$(PackageVersion).nupkg",
-                "Caravela.Framework.DesignTime.Contracts.$(PackageVersion).nupkg"
-            };
+var commandApp = new CommandApp();
 
-            var publicPublishing = new NugetPublishTarget(
-                Pattern.Empty.Add( publicPackages ),
-                privateSource,
-                publicSource );
+commandApp.AddProductCommands( product );
 
-            var product = new Product
-            {
-                ProductName = "Caravela",
-                Solutions = ImmutableArray.Create<Solution>(
-                    new DotNetSolution( "Caravela.sln" )
-                    {
-                        SupportsTestCoverage = true,
-                        CanFormatCode = true,
-                        FormatExclusions = ImmutableArray.Create(
-                            "Tests\\Caravela.Framework.Tests.Integration\\Tests\\**\\*",
-                            "Tests\\Caravela.Framework.Tests.Integration.Internals\\Tests\\**\\*" )
-                    },
-                    new DotNetSolution( "Tests\\Caravela.Framework.TestApp\\Caravela.Framework.TestApp.sln" )
-                    {
-                        IsTestOnly = true
-                    } ),
-                PublishingTargets = ImmutableArray.Create<PublishingTarget>( publicPublishing ),
-                Dependencies = ImmutableArray.Create(
-                    Dependencies.PostSharpEngineering,
-                    Dependencies.CaravelaCompiler )
-            };
-
-            var commandApp = new CommandApp();
-            commandApp.AddProductCommands( product );
-
-            return commandApp.Run( args );
-        }
-    }
-}
+return commandApp.Run( args );
