8df6abbde837d66a5db838f4a454100e627b7877
Antonín Procházka <antonin@postsharp.net>
2021-12-14 17:19:42 +0100

    #29560 Engineering updated.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -35,6 +35,7 @@ var product = new Product
         "Metalama.Framework.DesignTime.Contracts.$(PackageVersion).nupkg" ),
     Dependencies = ImmutableArray.Create(
         Dependencies.PostSharpEngineering,
+        Dependencies.Roslyn,
         Dependencies.MetalamaCompiler,
         Dependencies.PostSharpBackstageSettings )
 };
