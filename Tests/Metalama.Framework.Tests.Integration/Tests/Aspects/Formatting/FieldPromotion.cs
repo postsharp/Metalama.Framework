6bb3d73ada1e2c6c74ede7dc94b250adfc3624b4
Daniel Balas <daniel@postsharp.net>
2022-02-16 12:30:02 +0100

    #29728 Fix test project file, test.

---
+++ Tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Formatting/FieldPromotion.cs
@@ -0,0 +1,37 @@
+﻿using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+using Metalama.Framework.Code.SyntaxBuilders;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Formatting.FieldPromotion
+{
+    public class TestAspect : OverrideFieldOrPropertyAspect
+    {
+        public override dynamic? OverrideProperty 
+        { 
+            get
+            {
+                Console.WriteLine("Aspect code");
+                return meta.Proceed();
+            }
+
+            set
+            {
+                Console.WriteLine("Aspect code");
+                meta.Proceed();
+            }
+        }
+    }
+
+    // <target>
+    public class Target
+    {
+        [TestAspect]
+        private int _myField;
+    }
+}
