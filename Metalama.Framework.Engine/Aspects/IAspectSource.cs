29cca5854ef28cefc18e1b70e2011d806d0a7273
Gael Fraiteur <gael@postsharp.net>
2022-03-13 19:05:01 +0100

    Formatting.

--- Metalama.Framework.Engine/Aspects/IAspectSource.cs
+++ Metalama.Framework.Engine/Aspects/IAspectSource.cs
@@ -2,13 +2,9 @@
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
 using Metalama.Framework.Aspects;
-using Metalama.Framework.Code;
 using Metalama.Framework.Engine.CodeModel;
-using Metalama.Framework.Engine.CodeModel.References;
 using Metalama.Framework.Engine.Diagnostics;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using System.Threading;
 
 namespace Metalama.Framework.Engine.Aspects;
@@ -30,19 +26,4 @@ internal interface IAspectSource
         IAspectClass aspectClass,
         IDiagnosticAdder diagnosticAdder,
         CancellationToken cancellationToken );
-}
-
-internal readonly struct AspectSourceResult
-{
-    public IEnumerable<AspectInstance> AspectInstances { get; }
-
-    public IEnumerable<Ref<IDeclaration>> Exclusions { get; }
-
-    public static AspectSourceResult Empty => new( null );
-
-    public AspectSourceResult( IEnumerable<AspectInstance>? aspectInstances, IEnumerable<Ref<IDeclaration>>? exclusions = null )
-    {
-        this.AspectInstances = aspectInstances ?? Enumerable.Empty<AspectInstance>();
-        this.Exclusions = exclusions ?? Enumerable.Empty<Ref<IDeclaration>>();
-    }
 }
