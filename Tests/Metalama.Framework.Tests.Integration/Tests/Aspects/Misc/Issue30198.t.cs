e2fb76a6476af92318933f3870cb274f54d912b4
Gael Fraiteur <gael@postsharp.net>
2022-04-14 13:05:21 +0200

    #30198 Cannot reference a custom attribute in an 'if' or 'foreach' clause of a template without adding [CompileTime] to the custom attribute.

---
+++ Tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Misc/Issue30198.t.cs
@@ -0,0 +1,12 @@
+internal class Target
+{
+    [Aspect]
+    private void M( int x )
+    {
+    global::System.Console.WriteLine("Hello, world.");
+    global::System.Console.WriteLine($"Param Target.M(int)/x");
+    var n = "ExcludeLoggingAttribute";
+            _ = x;
+    return;
+    }
+}
