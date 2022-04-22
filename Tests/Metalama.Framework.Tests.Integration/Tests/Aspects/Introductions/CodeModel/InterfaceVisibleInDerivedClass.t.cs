536ef0eda4b7c278a6cce3ca5bebe10c02aae448
Daniel Balas <daniel@postsharp.net>
2022-04-22 20:10:40 +0200

    #30151 Additional test.

---
+++ Tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Introductions/CodeModel/InterfaceVisibleInDerivedClass.t.cs
@@ -0,0 +1,11 @@
+// <target>
+[Introduction]
+public class BaseClass : global::Metalama.Framework.Tests.Integration.TestInputs.Aspects.Introductions.CodeModel.InterfaceVisibleInDerivedClass.IInterface
+{ 
+}
+
+// <target>
+[Introduction]
+public class DerivedClass : BaseClass 
+{
+}
