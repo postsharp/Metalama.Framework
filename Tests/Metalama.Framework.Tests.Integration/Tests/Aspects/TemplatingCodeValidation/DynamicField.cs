f215b79a794edad4bff89d1cf6931cc4ec6e0ac8
Gael Fraiteur <gael@postsharp.net>
2022-05-07 12:18:00 +0200

    Fixed warning.

--- Tests/Metalama.Framework.Tests.Integration/Tests/Aspects/TemplatingCodeValidation/DynamicField.cs
+++ Tests/Metalama.Framework.Tests.Integration/Tests/Aspects/TemplatingCodeValidation/DynamicField.cs
@@ -3,6 +3,8 @@ using Metalama.Framework.Aspects;
 
 namespace Metalama.Framework.Tests.Integration.Tests.Aspects.TemplatingCodeValidation.DynamicField;
 
+#pragma warning disable CS0414
+
 public class TheAspect : TypeAspect
 {
     private dynamic f1 = null!;
