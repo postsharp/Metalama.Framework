3e47a86008a4b8065a3271dd70499cc1b7b6f1e1
Daniel Balas <daniel@postsharp.net>
2022-05-11 09:38:44 +0200

    #30345 Repro.

---
+++ Tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Async/AsyncTemplate/AsyncMethod_CrossAssembly.Dependency.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Metalama.Framework;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.Tests.Integration.Aspects.Async.AsyncTemplate.AsyncMethod_CrossAssembly
+{
+    public class Aspect : OverrideMethodAspect
+    {
+        public override dynamic? OverrideMethod()
+        {
+            return meta.Proceed();
+        }
+        
+        public override async System.Threading.Tasks.Task<dynamic?> OverrideAsyncMethod()
+        {
+            await Task.Yield();
+            var result = await meta.Proceed();
+            Console.WriteLine($"result={result}");
+            return result;
+            
+        }
+    }
+}
