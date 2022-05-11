3e99ad5024e2b82c1aba4f6e99999d2d138af51e
Gael Fraiteur <gael@postsharp.net>
2022-05-11 11:42:01 +0200

    Fixes after the merge.

--- Metalama.Framework.Engine/Advices/IntroducePropertyAdvice.cs
+++ Metalama.Framework.Engine/Advices/IntroducePropertyAdvice.cs
@@ -67,9 +67,9 @@ namespace Metalama.Framework.Engine.Advices
 
             // TODO: Indexers.
 
-            this.MemberBuilder.Type = (this.Template.Declaration?.Type ?? this._getTemplate.Declaration?.ReturnType).AssertNotNull();
-            this.MemberBuilder.Accessibility = 
-                (this.Template.Declaration?.Accessibility ?? this._getTemplate.Declaration?.Accessibility ?? this._setTemplate.Declaration?.Accessibility).AssertNotNull();
+            this.MemberBuilder.Type = (this.Template.Declaration?.Type ?? this._getTemplate.Template.Declaration?.ReturnType).AssertNotNull();
+            this.MemberBuilder.Accessibility =
+                (this.Template.Declaration?.Accessibility ?? this._getTemplate.Template.Declaration?.Accessibility ?? this._setTemplate.Template.Declaration?.Accessibility).AssertNotNull();
 
             if ( this.Template.IsNotNull )
             {
