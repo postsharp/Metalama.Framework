2cb67f1d25c4db90656d644bc3520bfb4ba5ece4
Gael Fraiteur <gael@postsharp.net>
2022-06-07 11:36:41 +0200

    #30544 Test framework: concurrent processing

---
+++ Metalama.TestFramework/XunitFramework/Metrics.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
+// This project is not open source. Please see the LICENSE.md file in the repository root for details.
+
+using System;
+using System.Threading;
+
+namespace Metalama.TestFramework.XunitFramework;
+
+internal class Metrics
+{
+    private int _testsRun;
+    private int _testFailed;
+    private int _testSkipped;
+    private int _testsRemaining;
+    private int _testsStarted;
+
+    private Metrics? _parent;
+    private long _executionTime;
+
+    public Metrics( Metrics? parent = null )
+    {
+        this._parent = parent;
+    }
+
+    public int TestsRun => this._testsRun;
+
+    public int TestFailed => this._testFailed;
+
+    public int TestSkipped => this._testSkipped;
+
+    public decimal ExecutionTime => (decimal) TimeSpan.FromMilliseconds( this._executionTime ).TotalSeconds;
+
+    public event Action? Started;
+
+    public event Action? Finished;
+
+    public void OnTestsDiscovered( int count )
+    {
+        Interlocked.Add( ref this._testsRemaining, count );
+        this._parent?.OnTestsDiscovered( count );
+
+        if ( count == 0 )
+        {
+            this.Finished?.Invoke();
+        }
+    }
+
+    public void OnTestStarted()
+    {
+        if ( Interlocked.Increment( ref this._testsStarted ) == 1 )
+        {
+            this.Started?.Invoke();
+        }
+
+        this._parent?.OnTestStarted();
+    }
+
+    private void OnTestFinished()
+    {
+        if ( Interlocked.Decrement( ref this._testsRemaining ) == 0 )
+        {
+            this.Finished?.Invoke();
+        }
+
+        this._parent?.OnTestFinished();
+    }
+
+    public void OnTestSucceeded( TimeSpan duration )
+    {
+        Interlocked.Increment( ref this._testsRun );
+        Interlocked.Add( ref this._executionTime, (long) duration.TotalMilliseconds );
+        this._parent?.OnTestSucceeded( duration );
+        this.OnTestFinished();
+    }
+
+    public void OnTestFailed( TimeSpan duration )
+    {
+        Interlocked.Increment( ref this._testFailed );
+        Interlocked.Add( ref this._executionTime, (long) duration.TotalMilliseconds );
+        this._parent?.OnTestFailed( duration );
+        this.OnTestFinished();
+    }
+
+    public void OnTestSkipped()
+    {
+        Interlocked.Increment( ref this._testSkipped );
+        this._parent?.OnTestSkipped();
+        this.OnTestFinished();
+    }
+}
--- Metalama.TestFramework/XunitFramework/TestExecutor.cs
+++ Metalama.TestFramework/XunitFramework/TestExecutor.cs
@@ -4,10 +4,12 @@
 using Metalama.Framework.Engine.Pipeline;
 using Metalama.Framework.Engine.Testing;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -55,8 +57,47 @@ namespace Metalama.TestFramework.XunitFramework
 
             var collections = testCases.GroupBy( t => t.TestMethod.TestClass.TestCollection );
 
+            var tasks = new ConcurrentDictionary<Task, Task>();
+            var semaphore = new SemaphoreSlim( executionOptions.MaxParallelThreadsOrDefault() );
+
             foreach ( var collection in collections )
             {
+                var collectionMetrics = new Metrics();
+
+                collectionMetrics.Started += () =>
+                {
+                    executionMessageSink.OnMessage( new TestCollectionStarting( collection, collection.Key ) );
+
+                    executionMessageSink.OnMessage(
+                        new TestAssemblyStarting(
+                            collection,
+                            collection.Key.TestAssembly,
+                            DateTime.Now,
+                            "CompileTime",
+                            "CompileTime" ) );
+                };
+
+                collectionMetrics.Finished += () =>
+                {
+                    executionMessageSink.OnMessage(
+                        new TestCollectionFinished(
+                            collection,
+                            collection.Key,
+                            collectionMetrics.ExecutionTime,
+                            collectionMetrics.TestsRun,
+                            collectionMetrics.TestFailed,
+                            collectionMetrics.TestSkipped ) );
+
+                    executionMessageSink.OnMessage(
+                        new TestAssemblyFinished(
+                            collection,
+                            collection.Key.TestAssembly,
+                            collectionMetrics.ExecutionTime,
+                            collectionMetrics.TestsRun,
+                            collectionMetrics.TestFailed,
+                            collectionMetrics.TestSkipped ) );
+                };
+
                 var projectMetadata = TestAssemblyMetadataReader.GetMetadata( collection.Key.TestAssembly.Assembly );
 
                 if ( projectMetadata.MustLaunchDebugger && !hasLaunchedDebugger )
@@ -67,156 +108,149 @@ namespace Metalama.TestFramework.XunitFramework
 
                 var projectReferences = projectMetadata.ToProjectReferences();
 
-                // Creates the set of references. Include all project references plus the project itself.
-
-                executionMessageSink.OnMessage( new TestCollectionStarting( collection, collection.Key ) );
-
-                executionMessageSink.OnMessage(
-                    new TestAssemblyStarting(
-                        collection,
-                        collection.Key.TestAssembly,
-                        DateTime.Now,
-                        "CompileTime",
-                        "CompileTime" ) );
-
-                int collectionTestsRun = 0, collectionTestFailed = 0, collectionTestSkipped = 0;
-                var collectionStopwatch = Stopwatch.StartNew();
-
-                try
+                foreach ( var type in collection.GroupBy( c => c.TestMethod.TestClass ) )
                 {
-                    foreach ( var type in collection.GroupBy( c => c.TestMethod.TestClass ) )
+                    var typeMetrics = new Metrics( collectionMetrics );
+                    typeMetrics.Started += () => executionMessageSink.OnMessage( new TestClassStarting( type, type.Key ) );
+
+                    typeMetrics.Finished += () => executionMessageSink.OnMessage(
+                        new TestClassFinished(
+                            type,
+                            type.Key,
+                            typeMetrics.ExecutionTime,
+                            typeMetrics.TestsRun,
+                            typeMetrics.TestFailed,
+                            typeMetrics.TestSkipped ) );
+
+                    typeMetrics.OnTestsDiscovered( type.Count() );
+
+                    foreach ( var testCase in type )
                     {
-                        int typeTestsRun = 0, typeTestFailed = 0, typeTestSkipped = 0;
-                        var typeStopwatch = Stopwatch.StartNew();
+                        var testMetrics = new Metrics( typeMetrics );
+                        var test = new Test( testCase );
+                        var logger = new TestOutputHelper();
 
-                        executionMessageSink.OnMessage( new TestClassStarting( type, type.Key ) );
-
-                        try
+                        testMetrics.Started += () =>
                         {
-                            foreach ( var testCase in type )
-                            {
-                                int testsRun = 0, testFailed = 0, testSkipped = 0;
-                                var test = new Test( testCase );
+                            executionMessageSink.OnMessage( new TestMethodStarting( new[] { testCase }, testCase.TestMethod ) );
+                            executionMessageSink.OnMessage( new TestCaseStarting( testCase ) );
+                            executionMessageSink.OnMessage( new TestStarting( test ) );
+                        };
 
-                                executionMessageSink.OnMessage( new TestMethodStarting( new[] { testCase }, testCase.TestMethod ) );
-                                executionMessageSink.OnMessage( new TestCaseStarting( testCase ) );
-                                executionMessageSink.OnMessage( new TestStarting( test ) );
+                        testMetrics.Finished += () =>
 
-                                var testStopwatch = Stopwatch.StartNew();
-
-                                var logger = new TestOutputHelper();
-
-                                try
-                                {
-                                    using var testOptions = new TestProjectOptions( plugIns: projectReferences.PlugIns );
-
-                                    var serviceProvider = ServiceProviderFactory.GetServiceProvider( testOptions.PathOptions ).WithService( testOptions );
-
-                                    var testInput = TestInput.FromFile( this._factory.ProjectProperties, directoryOptionsReader, testCase.UniqueID );
-
-                                    if ( testInput.IsSkipped )
-                                    {
-                                        executionMessageSink.OnMessage( new TestSkipped( test, testInput.SkipReason ) );
-
-                                        testSkipped++;
-                                        typeTestSkipped++;
-                                        collectionTestSkipped++;
-                                    }
-                                    else
-                                    {
-                                        var testRunner = TestRunnerFactory.CreateTestRunner(
-                                            testInput,
-                                            serviceProvider,
-                                            projectReferences,
-                                            logger );
-
-                                        Task.Run( () => testRunner.RunAndAssertAsync( testInput ) ).Wait();
-
-                                        executionMessageSink.OnMessage( new TestPassed( test, testStopwatch.GetSeconds(), logger.ToString() ) );
-
-                                        testsRun++;
-                                        typeTestsRun++;
-                                        collectionTestsRun++;
-                                    }
-                                }
-                                catch ( Exception e )
-                                {
-                                    testFailed++;
-                                    typeTestFailed++;
-                                    collectionTestFailed++;
-
-                                    IFailureInformation failureInformation;
-
-                                    if ( e is AggregateException { InnerExceptions: { Count: 1 } } aggregateException )
-                                    {
-                                        failureInformation = ExceptionUtility.ConvertExceptionToFailureInformation( aggregateException.InnerException );
-                                    }
-                                    else
-                                    {
-                                        failureInformation = ExceptionUtility.ConvertExceptionToFailureInformation( e );
-                                    }
-
-                                    executionMessageSink.OnMessage(
-                                        new TestFailed(
-                                            test,
-                                            testStopwatch.GetSeconds(),
-                                            logger.ToString(),
-                                            failureInformation.ExceptionTypes,
-                                            failureInformation.Messages,
-                                            failureInformation.StackTraces,
-                                            failureInformation.ExceptionParentIndices ) );
-                                }
-                                finally
-                                {
-                                    executionMessageSink.OnMessage( new TestFinished( test, testStopwatch.GetSeconds(), logger.ToString() ) );
-
-                                    executionMessageSink.OnMessage(
-                                        new TestCaseFinished( testCase, testStopwatch.GetSeconds(), testsRun, testFailed, testSkipped ) );
-
-                                    executionMessageSink.OnMessage(
-                                        new TestMethodFinished(
-                                            new[] { testCase },
-                                            testCase.TestMethod,
-                                            testStopwatch.GetSeconds(),
-                                            testsRun,
-                                            testFailed,
-                                            testSkipped ) );
-                                }
-                            }
-                        }
-                        finally
                         {
+                            executionMessageSink.OnMessage( new TestFinished( test, testMetrics.ExecutionTime, logger.ToString() ) );
+
                             executionMessageSink.OnMessage(
-                                new TestClassFinished(
-                                    type,
-                                    type.Key,
-                                    typeStopwatch.GetSeconds(),
-                                    typeTestsRun,
-                                    typeTestFailed,
-                                    typeTestSkipped ) );
+                                new TestCaseFinished(
+                                    testCase,
+                                    testMetrics.ExecutionTime,
+                                    testMetrics.TestsRun,
+                                    testMetrics.TestFailed,
+                                    testMetrics.TestSkipped ) );
+
+                            executionMessageSink.OnMessage(
+                                new TestMethodFinished(
+                                    new[] { testCase },
+                                    testCase.TestMethod,
+                                    testMetrics.ExecutionTime,
+                                    testMetrics.TestsRun,
+                                    testMetrics.TestFailed,
+                                    testMetrics.TestSkipped ) );
+                        };
+
+                        var task = Task.Run(
+                            () => this.RunTestAsync( executionMessageSink, projectReferences, directoryOptionsReader, testCase, test, testMetrics, logger ) );
+
+                        if ( executionOptions.DisableParallelizationOrDefault() )
+                        {
+                            task.Wait();
+                        }
+                        else
+                        {
+                            // Throttle execution thanks to the semaphore.
+                            semaphore.Wait();
+
+                            // When the task is over, release the semaphore.
+                            task.ContinueWith( t => { semaphore.Release(); } );
+
+                            tasks.TryAdd( task, task );
                         }
                     }
                 }
-                finally
-                {
-                    executionMessageSink.OnMessage(
-                        new TestCollectionFinished(
-                            collection,
-                            collection.Key,
-                            collectionStopwatch.GetSeconds(),
-                            collectionTestsRun,
-                            collectionTestFailed,
-                            collectionTestSkipped ) );
+            }
 
-                    executionMessageSink.OnMessage(
-                        new TestAssemblyFinished(
-                            collection,
-                            collection.Key.TestAssembly,
-                            collectionStopwatch.GetSeconds(),
-                            collectionTestsRun,
-                            collectionTestFailed,
-                            collectionTestSkipped ) );
+            // Wait for all tasks to complete and catch exceptions.
+            Task.WhenAll( tasks.Keys ).Wait();
+        }
+
+        private async Task RunTestAsync(
+            IMessageSink executionMessageSink,
+            TestProjectReferences projectReferences,
+            TestDirectoryOptionsReader directoryOptionsReader,
+            ITestCase testCase,
+            Test test,
+            Metrics testMetrics,
+            TestOutputHelper logger )
+        {
+            var testStopwatch = Stopwatch.StartNew();
+
+            try
+            {
+                testMetrics.OnTestStarted();
+
+                using var testOptions = new TestProjectOptions( plugIns: projectReferences.PlugIns );
+
+                var serviceProvider = ServiceProviderFactory.GetServiceProvider( testOptions.PathOptions ).WithService( testOptions );
+
+                var testInput = TestInput.FromFile( this._factory.ProjectProperties, directoryOptionsReader, testCase.UniqueID );
+
+                if ( testInput.IsSkipped )
+                {
+                    executionMessageSink.OnMessage( new TestSkipped( test, testInput.SkipReason ) );
+
+                    testMetrics.OnTestSkipped();
                 }
+                else
+                {
+                    var testRunner = TestRunnerFactory.CreateTestRunner(
+                        testInput,
+                        serviceProvider,
+                        projectReferences,
+                        logger );
+
+                    await testRunner.RunAndAssertAsync( testInput );
+
+                    testMetrics.OnTestSucceeded( testStopwatch.Elapsed );
+
+                    executionMessageSink.OnMessage( new TestPassed( test, testMetrics.ExecutionTime, logger.ToString() ) );
+                }
+            }
+            catch ( Exception e )
+            {
+                testMetrics.OnTestFailed( testStopwatch.Elapsed );
+
+                IFailureInformation failureInformation;
+
+                if ( e is AggregateException { InnerExceptions: { Count: 1 } } aggregateException )
+                {
+                    failureInformation = ExceptionUtility.ConvertExceptionToFailureInformation( aggregateException.InnerException );
+                }
+                else
+                {
+                    failureInformation = ExceptionUtility.ConvertExceptionToFailureInformation( e );
+                }
+
+                executionMessageSink.OnMessage(
+                    new TestFailed(
+                        test,
+                        testMetrics.ExecutionTime,
+                        logger.ToString(),
+                        failureInformation.ExceptionTypes,
+                        failureInformation.Messages,
+                        failureInformation.StackTraces,
+                        failureInformation.ExceptionParentIndices ) );
             }
         }
     }
