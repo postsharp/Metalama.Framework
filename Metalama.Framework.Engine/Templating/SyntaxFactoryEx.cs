62ac6c5b3c98ce435beae2381c38b811590bf240
Gael Fraiteur <gael@postsharp.net>
2022-06-17 15:01:23 +0200

    Fixed generation of boolean literals.

--- Metalama.Framework.Engine/Templating/SyntaxFactoryEx.cs
+++ Metalama.Framework.Engine/Templating/SyntaxFactoryEx.cs
@@ -68,6 +68,7 @@ namespace Metalama.Framework.Engine.Templating
                 double s => LiteralExpression( s, options ),
                 float s => LiteralExpression( s, options ),
                 decimal s => LiteralExpression( s, options ),
+                bool b => LiteralExpression( b, options ),
                 _ => null
             };
 
@@ -131,6 +132,9 @@ namespace Metalama.Framework.Engine.Templating
 
         public static LiteralExpressionSyntax LiteralExpression( char c, ObjectDisplayOptions options = ObjectDisplayOptions.None )
             => SyntaxFactory.LiteralExpression( SyntaxKind.CharacterLiteralExpression, LiteralImpl( c, options ) );
+        
+        public static LiteralExpressionSyntax LiteralExpression( bool b, ObjectDisplayOptions options = ObjectDisplayOptions.None )
+            => SyntaxFactory.LiteralExpression( b ? SyntaxKind.TrueLiteralExpression : SyntaxKind.FalseLiteralExpression );
 
         private static ExpressionSyntax EmptyExpression => SyntaxFactory.IdentifierName( SyntaxFactory.MissingToken( SyntaxKind.IdentifierToken ) );
 
