74b175a46d58734622d85e5ffcbaaac0db305431
Gael Fraiteur <gael@postsharp.net>
2022-06-18 11:18:40 +0200

    Fixed test.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Misc/FieldOrPropertyValueShortcuts.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Misc/FieldOrPropertyValueShortcuts.t.cs
@@ -1,1 +1,13 @@
-// TODO: Replace this file with the correct transformed code. See the test output for the actual transformed code.
+[MyAspect]
+internal class C
+{
+    int _instanceField = 5;
+    private static int _staticField = 6;
+
+
+    public void Method()
+    {
+        this._instanceField = this._instanceField;
+        global::Metalama.Framework.Tests.Integration.Tests.Aspects.Misc.FieldOrPropertyValueShortcuts.C._staticField = global::Metalama.Framework.Tests.Integration.Tests.Aspects.Misc.FieldOrPropertyValueShortcuts.C._staticField;
+    }
+}
