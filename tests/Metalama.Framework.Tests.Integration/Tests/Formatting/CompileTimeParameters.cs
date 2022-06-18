95211c19c395e9a6353b1decd17b56dc22762d97
Gael Fraiteur <gael@postsharp.net>
2022-06-18 23:50:40 +0200

    #30623 Syntax highlighting: template parameters.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Formatting/CompileTimeParameters.cs
@@ -0,0 +1,14 @@
+﻿using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Formatting.CompileTimeParameters
+{
+    internal class MyAspect : TypeAspect
+    {
+        [Template]
+        public T Template<[CompileTime] T>( IDeclaration d )
+        {
+            return default;
+        }
+    }
+}
