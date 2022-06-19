cdb980dc18004a90d066edad778b4bd8f802b4af
Gael Fraiteur <gael@postsharp.net>
2022-06-19 06:41:25 +0200

    Fixed warning.

--- tests/Metalama.Framework.Tests.Integration/Tests/Formatting/CompileTimeParameters.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Formatting/CompileTimeParameters.cs
@@ -1,6 +1,8 @@
 ﻿using Metalama.Framework.Aspects;
 using Metalama.Framework.Code;
 
+#pragma warning disable CS8603
+
 namespace Metalama.Framework.Tests.Integration.Tests.Formatting.CompileTimeParameters
 {
     internal class MyAspect : TypeAspect
