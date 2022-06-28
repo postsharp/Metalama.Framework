f45af6e5b406951562906b068ef9364bdbacbf88
Jan Hlavac <jan@postsharp.net>
2022-06-28 14:04:43 +0200

    #30645 Added repro.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Misc/Issue30621.Dependency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Misc.Issue30621.Dependency
+{
+    internal class ClassC : ClassB
+    {
+    }
+}
---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Misc/Issue30621.cs
@@ -0,0 +1,31 @@
+﻿using Metalama.Framework.Aspects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Misc.Issue30621
+{
+    public class ClassA
+    {
+        [Log]
+        public void Foo()
+        {
+            Console.WriteLine( "Bar" );
+        }
+    }
+
+    public class ClassB : ClassA
+    {
+    }
+
+    public class LogAttribute : OverrideMethodAspect
+    {
+        public override dynamic? OverrideMethod()
+        {
+            Console.WriteLine( "Simple log" );
+            return meta.Proceed();
+        }
+    }
+}
