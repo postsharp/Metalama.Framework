fd387e9424e930f6f70228c3c40021f4166ebd81
Jan Hlavac <jan@postsharp.net>
2022-07-01 15:48:24 +0200

    #30634,#30635 Added fabrics tests.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Fabrics/Target_Record_TypeFabricAddAdvice.cs
@@ -0,0 +1,33 @@
+using System;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Fabrics;
+
+namespace Metalama.Framework.Tests.PublicPipeline.Aspects.Fabrics.TypeFabricAddAdvice
+{
+    // <target>
+    internal record TargetRecord
+    {
+        private int Method1( int a ) => a;
+
+        private string Method2( string s ) => s;
+
+        private class Fabric : TypeFabric
+        {
+            public override void AmendType( ITypeAmender amender )
+            {
+                foreach (var method in amender.Type.Methods)
+                {
+                    amender.Advices.Override( method, nameof(Template) );
+                }
+            }
+
+            [Template]
+            private dynamic? Template()
+            {
+                Console.WriteLine( "overridden" );
+
+                return meta.Proceed();
+            }
+        }
+    }
+}
