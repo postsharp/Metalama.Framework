3ac88b0c7275bd9f69a46fe5b48283504f23716c
Gael Fraiteur <gael@postsharp.net>
2022-07-19 18:22:52 +0200

    Fixes warning.

--- Metalama.Framework.Engine/Advising/TemplateMember.cs
+++ Metalama.Framework.Engine/Advising/TemplateMember.cs
@@ -1,4 +1,7 @@
-﻿using Metalama.Framework.Aspects;
+﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
+// This project is not open source. Please see the LICENSE.md file in the repository root for details.
+
+using Metalama.Framework.Aspects;
 using Metalama.Framework.Code;
 using Metalama.Framework.Engine.CompileTime;
 using System.Linq;
