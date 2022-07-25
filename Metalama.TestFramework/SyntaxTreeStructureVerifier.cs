1bfcd7228ec97a04e845554fa8d1159ea263055a
Daniel Balas <daniel@postsharp.net>
2022-07-25 12:18:41 +0200

    Remove using.

--- Metalama.TestFramework/SyntaxTreeStructureVerifier.cs
+++ Metalama.TestFramework/SyntaxTreeStructureVerifier.cs
@@ -7,7 +7,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Text;
 
 namespace Metalama.TestFramework
