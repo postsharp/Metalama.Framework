96eea4b93637ba246d24f7042d8df14971fd1fe1
Jan Hlavac <jan@postsharp.net>
2022-09-02 12:15:20 +0200

    #30823 Codestyle formatting applied.

--- Metalama.Framework.Engine/SyntaxSerialization/ExpressionBuilderSerializer.cs
+++ Metalama.Framework.Engine/SyntaxSerialization/ExpressionBuilderSerializer.cs
@@ -1,5 +1,4 @@
-// Copyright (c) SharpCrafters s.r.o. All rights reserved.
-// This project is not open source. Please see the LICENSE.md file in the repository root for details.
+// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using Metalama.Framework.Code.SyntaxBuilders;
 using Metalama.Framework.Engine.Templating.Expressions;
