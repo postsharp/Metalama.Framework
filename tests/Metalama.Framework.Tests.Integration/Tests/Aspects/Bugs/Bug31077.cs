7898b427e8aba4ef61325212b2307b6f703d5f9a
Daniel Balas <daniel@postsharp.net>
2022-09-16 15:34:02 +0200

    #31077 Repro.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/Bug31077.cs
@@ -0,0 +1,50 @@
+﻿#if TEST_OPTIONS
+// @KeepDisabledCode
+#endif
+
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Bugs.Bug31077
+{
+    public class TestAspect : MethodAspect
+    {
+        public override void BuildAspect(IAspectBuilder<IMethod> builder)
+        {
+            builder.Advice.Override(builder.Target, nameof(OverrideMethod));
+            builder.Advice.IntroduceMethod(builder.Target.DeclaringType, nameof(IntroducedTemplate), buildMethod: b => { b.Name = builder.Target.Name + "_Introduced"; });
+        }
+
+        [Template]
+        public dynamic? OverrideMethod()
+        {
+            return meta.Proceed();
+        }
+
+        [Template]
+        public void IntroducedTemplate([CompileTime] IMethod target)
+        {
+            _ = target.Invokers.Base!.Invoke(meta.This);
+        }
+    }
+
+    // <target>
+    public class TargetClass<T> : IEnumerable<T>
+    {
+        [TestAspect]
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Enumerable.Empty<T>().GetEnumerator();
+        }
+
+        [TestAspect]
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
