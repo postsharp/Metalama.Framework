3027d475c443824896157adb14bd73731bed110c
Daniel Balas <daniel@postsharp.net>
2022-09-17 07:12:18 +0200

    #31077 Fix linker test.

--- tests/Metalama.Framework.Tests.Integration.Internals/Tests/Linker/Naming/InterfaceMembers.t.cs
+++ tests/Metalama.Framework.Tests.Integration.Internals/Tests/Linker/Naming/InterfaceMembers.t.cs
@@ -1,43 +1,54 @@
-    class Target : ITest
+using System;
+
+class Target : ITest
+{
+    int ITest.Foo
+    {
+        get
         {
-            int ITest.Foo 
-    { get
-    { 
-            return this.Foo_Source;
-    
-    }
-    set
-    { 
-            this.Foo_Source = value;
-    
-    }
-    }
-    
-    private int Foo_Source { get; set; }
-    
-            int ITest.Bar()
-            {
-        return this.Bar_Source();
-            }
-    
-    private int Bar_Source()
-            {
-                return 42;
-            }
-    
-    
-            event EventHandler ITest.Quz
-            {
-                add {         this.Quz_Source += value;
-    }
-                remove {         this.Quz_Source -= value;
-    }
-            }
-    
-    private event EventHandler Quz_Source
-            {
-                add { }
-                remove { }
-            }
-    
+            return this.Metalama_Framework_Tests_Integration_Tests_Linker_Naming_InterfaceMembers_ITest_Foo_Source;
+
+
         }
+        set
+        {
+            this.Metalama_Framework_Tests_Integration_Tests_Linker_Naming_InterfaceMembers_ITest_Foo_Source = value;
+
+        }
+    }
+
+    private int Metalama_Framework_Tests_Integration_Tests_Linker_Naming_InterfaceMembers_ITest_Foo_Source
+    { get; set; }
+    int ITest.Bar()
+    {
+        return this.Metalama_Framework_Tests_Integration_Tests_Linker_Naming_InterfaceMembers_ITest_Bar_Source();
+    }
+
+    private int Metalama_Framework_Tests_Integration_Tests_Linker_Naming_InterfaceMembers_ITest_Bar_Source()
+    {
+        return 42;
+    }
+
+
+    event EventHandler ITest.Quz
+    {
+        add
+        {
+            this.Metalama_Framework_Tests_Integration_Tests_Linker_Naming_InterfaceMembers_ITest_Quz_Source += value;
+
+
+        }
+        remove
+        {
+            this.Metalama_Framework_Tests_Integration_Tests_Linker_Naming_InterfaceMembers_ITest_Quz_Source -= value;
+
+        }
+    }
+
+    private event EventHandler Metalama_Framework_Tests_Integration_Tests_Linker_Naming_InterfaceMembers_ITest_Quz_Source
+    {
+        add { }
+        remove { }
+    }
+
+}
