a6ddb883deaeee27a55583d580a7f1b44bc2e03e
Daniel Balas <daniel@postsharp.net>
2022-09-26 11:03:20 +0200

    Fixes of NoCommerce bugs.

--- Metalama.Framework.Engine/Linking/Inlining/InlinerProvider.cs
+++ Metalama.Framework.Engine/Linking/Inlining/InlinerProvider.cs
@@ -1,5 +1,9 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
+#if TEST_OPTIONS
+// @Skipped(#31108)
+#endif
+
 using Microsoft.CodeAnalysis;
 using System.Linq;
 
