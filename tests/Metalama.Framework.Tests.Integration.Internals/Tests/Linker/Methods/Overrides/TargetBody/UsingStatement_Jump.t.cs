6e5cbd7474076e5f231459f17ceac498a3b9743a
Gael Fraiteur <gael@postsharp.net>
2022-09-27 14:53:27 +0200

    Fixed test.

--- tests/Metalama.Framework.Tests.Integration.Internals/Tests/Linker/Methods/Overrides/TargetBody/UsingStatement_Jump.t.cs
+++ tests/Metalama.Framework.Tests.Integration.Internals/Tests/Linker/Methods/Overrides/TargetBody/UsingStatement_Jump.t.cs
@@ -1,8 +1,23 @@
 class Target
 {
-  void Foo()
-  {
-    Console.WriteLine("Aspect");
-    Console.WriteLine("Original");
-  }
+    void Foo(int x)
+    {
+        Console.WriteLine("Before aspect");
+        if (x == 0)
+        {
+            goto __aspect_return_1;
+        }
+
+        Console.WriteLine("Before first dispose");
+        using (var z1 = new Disposable())
+        {
+            Console.WriteLine("Before double dispose");
+
+            using var z2 = new Disposable();
+            using var z3 = new Disposable();
+
+            Console.WriteLine("After dispose");
+        }
+    __aspect_return_1: Console.WriteLine("After aspect");
+    }
 }
