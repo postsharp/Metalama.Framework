d1be2bb204c9e16682426601a2ace599426c96ba
Daniel Balas <daniel@postsharp.net>
2022-10-03 09:57:43 +0200

    #31157 Minor fixes.

--- Metalama.Framework/Code/MethodExtensions.cs
+++ Metalama.Framework/Code/MethodExtensions.cs
@@ -1,10 +1,13 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
+using Metalama.Framework.Aspects;
+
 namespace Metalama.Framework.Code
 {
     /// <summary>
     /// Extension methods for the <see cref="IMethod"/> interface.
-    /// </summary>
+    /// </summary> 
+    [CompileTime]
     public static class MethodExtensions
     {
         /// <summary>
