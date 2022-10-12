deb7d6f26fa99ccc6524fd93bcf0237a2b42d18c
GaelFraiteur <gael@postsharp.net>
2022-10-12 17:08:51 +0200

    Marking metrics API as compile time.

--- Metalama.Framework/Metrics/Metrics.cs
+++ Metalama.Framework/Metrics/Metrics.cs
@@ -1,11 +1,14 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
+using Metalama.Framework.Aspects;
+
 namespace Metalama.Framework.Metrics
 {
     /// <summary>
     /// Exposes a <see cref="Get{TExtension}"/> method, which computes and returns a metric for an eligible object.
     /// </summary>
     /// <typeparam name="TMeasurable">The type extended with metrics.</typeparam>
+    [CompileTime]
     public readonly struct Metrics<TMeasurable>
         where TMeasurable : IMeasurable
     {
