16d07707d7e5e1f0cfab7006d389fd0688694565
GaelFraiteur <gael@postsharp.net>
2022-10-12 11:26:32 +0200

    Fixed namespace depth computation.

--- Metalama.Framework.Engine/CodeModel/CompilationModel.cs
+++ Metalama.Framework.Engine/CodeModel/CompilationModel.cs
@@ -288,11 +288,14 @@ namespace Metalama.Framework.Engine.CodeModel
                 case INamespace { IsExternal: true } ns:
                     throw new InvalidOperationException( $"Cannot compute the depth of '{ns.FullName}' because it is an external namespace." );
 
+                case INamespace { IsGlobalNamespace: true }:
+                    // We want the global namespace to be processed after all assembly references.
+                    return 2;
+
                 case INamespace ns:
                     {
-                        // We want the global namespace to be processed after all assembly references.
-                        // Then, we just count the number of dots in the name. This is the fastest approach.
-                        var depth = 2;
+                        // Then, we just count the number of components in the namespace, and we add the depth of the global namespace.
+                        var depth = 3;
 
                         foreach ( var c in ns.FullName )
                         {
