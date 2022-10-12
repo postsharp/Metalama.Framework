0111f83c60b93530eea7ccd4d8d4c7d79d896be7
GaelFraiteur <gael@postsharp.net>
2022-10-12 16:19:03 +0200

    Normalize test outputs.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Inheritance/CrossAssembly.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Inheritance/CrossAssembly.t.cs
@@ -1,17 +1,15 @@
 namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Inheritance.CrossAssembly
 {
-    public class C : I
+  public class C : I
+  {
+    public void Introduced()
     {
-
-
-public void Introduced()
-{
-}        
     }
-
-public class D : J {
-
-public void Introduced()
-{
-}}
+  }
+  public class D : J
+  {
+    public void Introduced()
+    {
+    }
+  }
 }
