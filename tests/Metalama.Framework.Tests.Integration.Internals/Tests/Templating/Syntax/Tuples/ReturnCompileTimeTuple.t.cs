827a1ce702e9ec38eb9d2405e7bd67bdde997322
GaelFraiteur <gael@postsharp.net>
2022-10-14 13:02:15 +0200

    Fixing test.

--- tests/Metalama.Framework.Tests.Integration.Internals/Tests/Templating/Syntax/Tuples/ReturnCompileTimeTuple.t.cs
+++ tests/Metalama.Framework.Tests.Integration.Internals/Tests/Templating/Syntax/Tuples/ReturnCompileTimeTuple.t.cs
@@ -1,4 +1,5 @@
 (int, Type) Method(int a)
 {
-  return ((global::System.Int32, global::System.Type))(1, typeof(global::System.Int32));
+    var t = (Count: 1, typeof(global::System.Int32));
+    return ((global::System.Int32, global::System.Type))(t.Count + 1, t.Item2);
 }
