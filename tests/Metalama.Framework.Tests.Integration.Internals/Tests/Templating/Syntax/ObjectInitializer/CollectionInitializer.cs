9f7838f3082876b62e6521bb15905a4531bb0c02
GaelFraiteur <gael@postsharp.net>
2022-10-14 20:37:16 +0200

    Tests for collection initializers.

---
+++ tests/Metalama.Framework.Tests.Integration.Internals/Tests/Templating/Syntax/ObjectInitializer/CollectionInitializer.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using Metalama.Framework.Aspects;
+using Metalama.TestFramework;
+
+namespace Metalama.Framework.Tests.Integration.Templating.CSharpSyntax.Misc.CollectionInitializer
+{
+    [CompileTime]
+    class Aspect
+    {
+        [TestTemplate]
+        dynamic? Template()
+        {
+            var x = new List<int> { 1, 2, 3 };
+            var y = new List<string> { meta.Target.Parameters.Count.ToString(), "a", "b" };
+            Console.WriteLine( string.Join( ", ", y ) );
+            var z = new List<int> { meta.Target.Parameters[0].Value, 2, 3 };
+            return default;
+        }
+    }
+
+    class TargetCode
+    {
+        int Method(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
