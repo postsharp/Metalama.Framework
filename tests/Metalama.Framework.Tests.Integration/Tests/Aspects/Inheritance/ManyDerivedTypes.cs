c35a10f0b27e1a5c3e03b6415287177860defd5c
GaelFraiteur <gael@postsharp.net>
2022-10-17 12:34:21 +0200

    #31236 DerivedTypeIndex.GetDerivedType throws InvalidOperationException: item has been visited twice.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Inheritance/ManyDerivedTypes.cs
@@ -0,0 +1,45 @@
+using System;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.Tests.PublicPipeline.Aspects.Inheritance.ManyDerivedTypes
+{
+    [Inherited]
+    internal class Aspect : TypeAspect
+    {
+        public override void BuildAspect( IAspectBuilder<INamedType> builder )
+        {
+            foreach (var m in builder.Target.Methods)
+            {
+                builder.Advice.Override( m, nameof(Template) );
+            }
+        }
+
+        [Template]
+        private dynamic? Template()
+        {
+            Console.WriteLine( "Overridden!" );
+
+            return meta.Proceed();
+        }
+    }
+
+    [Aspect]
+    public interface IBase { }
+
+    public interface ISub1 : IBase { }
+
+    public interface ISub2 : IBase { }
+
+    public interface IDerived : ISub1, ISub2 { }
+
+    public class BaseClass : IDerived
+    {
+        private void M() { }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        private void N() { }
+    }
+}
