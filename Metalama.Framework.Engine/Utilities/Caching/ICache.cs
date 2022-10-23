7e69117b561984178a0baed34162bb7cb871c3e9
GaelFraiteur <gael@postsharp.net>
2022-10-23 17:49:33 +0200

    Simplify WeakCache and implement (unused) AsyncWeakCache.

---
+++ Metalama.Framework.Engine/Utilities/Caching/ICache.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using System;
+
+namespace Metalama.Framework.Engine.Utilities.Caching;
+
+internal interface ICache<TKey, TValue>
+{
+    bool TryGetValue( TKey key, out TValue value );
+
+    TValue GetOrAdd( TKey key, Func<TKey, TValue> func );
+
+    bool TryAdd( TKey key, TValue value );
+}
--- Metalama.Framework.Engine/Utilities/Caching/WeakCache.cs
+++ Metalama.Framework.Engine/Utilities/Caching/WeakCache.cs
@@ -1,21 +1,9 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
-using Metalama.Framework.Code.Collections;
 using System;
-using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
-using System.Threading;
 
 namespace Metalama.Framework.Engine.Utilities.Caching;
-
-internal interface ICache<TKey, TValue>
-{
-    bool TryGetValue( TKey key, out TValue value );
-
-    TValue GetOrAdd( TKey key, Func<TKey, TValue> func );
-
-    bool TryAdd( TKey key, TValue value );
-}
 
 /// <summary>
 /// A cache based on <see cref="ConditionalWeakTable{TKey,TValue}"/>, which holds a weak reference to the key.
@@ -24,8 +12,6 @@ public sealed class WeakCache<TKey, TValue> : ICache<TKey, TValue>
     where TKey : class
 {
     private readonly ConditionalWeakTable<TKey, StrongBox<TValue>> _cache = new();
-    private readonly ConcurrentDictionary<TKey, object> _locks = new( ReferenceEqualityComparer<TKey>.Instance );
-    private readonly ThreadLocal<bool> _holdsLock = new();
 
     public bool TryGetValue( TKey key, out TValue value )
     {
@@ -51,81 +37,23 @@ public sealed class WeakCache<TKey, TValue> : ICache<TKey, TValue>
             return value;
         }
 
-        if ( this._holdsLock.Value )
+        lock ( key )
         {
-            // This is a recursion. The thread already holds the lock.
-
-            value = func( key );
-
-            if ( this._cache.TryGetValue( key, out var recursiveValue ) )
+            while ( true )
             {
-                return recursiveValue.Value!;
-            }
-            else
-            {
+                // We won the race.
+                // Create the new item.
+                value = func( key );
+
+                // The func may have added the same item to the cache.
+                if ( this.TryGetValue( key, out var recursiveValue ) )
+                {
+                    return recursiveValue;
+                }
+
                 this._cache.Add( key, new StrongBox<TValue>( value ) );
 
                 return value;
-            }
-        }
-        else
-        {
-            // There may a race of several threads wanting to add the item to the cache.
-            // We solve this problem by having a dictionary of locks. Each thread tries to have its own monitor to the dictionary.
-            // The thread that wins adds the item to the cache. The other threads have to wait.
-
-            // Create our own monitor and acquires it. We have to acquire it _before_ adding it to the dictionary of locks.
-            var ourMonitor = new object();
-
-            lock ( ourMonitor )
-            {
-                while ( true )
-                {
-                    var sharedMonitor = this._locks.GetOrAdd( key, ourMonitor );
-
-                    if ( sharedMonitor == ourMonitor )
-                    {
-                        // We won the race.
-                        try
-                        {
-                            // Create the new item.
-                            try
-                            {
-                                this._holdsLock.Value = true;
-                                value = func( key );
-                            }
-                            finally
-                            {
-                                this._holdsLock.Value = false;
-                            }
-
-                            // The func may have added the same item to the cache.
-                            if ( this.TryGetValue( key, out var recursiveValue ) )
-                            {
-                                return recursiveValue;
-                            }
-
-                            this._cache.Add( key, new StrongBox<TValue>( value ) );
-
-                            return value;
-                        }
-                        finally
-                        {
-                            this._locks.TryRemove( key, out _ );
-                        }
-                    }
-                    else
-                    {
-                        // We lost the race, so we have to wait.
-                        Monitor.Enter( sharedMonitor );
-                        Monitor.Exit( sharedMonitor );
-
-                        if ( this.TryGetValue( key, out value ) )
-                        {
-                            return value;
-                        }
-                    }
-                }
             }
         }
     }
@@ -137,44 +65,16 @@ public sealed class WeakCache<TKey, TValue> : ICache<TKey, TValue>
             return false;
         }
 
-        // There may a race of several threads wanting to add the item to the cache.
-        // We solve this problem by having a dictionary of locks. Each thread tries to have its own monitor to the dictionary.
-        // The thread that wins adds the item to the cache. The other threads have to wait.
-
-        // Create our own monitor and acquires it. We have to acquire it _before_ adding it to the dictionary of locks.
-        var ourMonitor = new object();
-
-        lock ( ourMonitor )
+        lock ( key )
         {
-            while ( true )
+            if ( this.TryGetValue( key, out _ ) )
             {
-                var sharedMonitor = this._locks.GetOrAdd( key, ourMonitor );
-
-                if ( sharedMonitor == ourMonitor )
-                {
-                    // We won the race.
-                    try
-                    {
-                        // Create the new item.
-                        this._cache.Add( key, new StrongBox<TValue>( value ) );
-
-                        return true;
-                    }
-                    finally
-                    {
-                        this._locks.TryRemove( key, out _ );
-                    }
-                }
-                else
-                {
-                    // We lost the race, but do not return until the other thread added the item.
-
-                    Monitor.Enter( sharedMonitor );
-                    Monitor.Exit( sharedMonitor );
-
-                    return false;
-                }
+                return false;
             }
+
+            this._cache.Add( key, new StrongBox<TValue>( value ) );
+
+            return true;
         }
     }
 }
