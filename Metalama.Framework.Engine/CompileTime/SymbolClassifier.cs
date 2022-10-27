34625a936377af7b68b5acc0a854b9c533fc02cf
GaelFraiteur <gael@postsharp.net>
2022-10-27 11:30:19 +0200

    Fixed warning.

--- Metalama.Framework.Engine/CompileTime/SymbolClassifier.cs
+++ Metalama.Framework.Engine/CompileTime/SymbolClassifier.cs
@@ -2,7 +2,6 @@
 
 using Metalama.Backstage.Diagnostics;
 using Metalama.Framework.Aspects;
-using Metalama.Framework.Code.Collections;
 using Metalama.Framework.Engine.CodeModel;
 using Metalama.Framework.Engine.Collections;
 using Metalama.Framework.Engine.Diagnostics;
