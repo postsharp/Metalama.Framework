dd6d22bf6bfadd55eb43768b5ca5de5cdcdae569
GaelFraiteur <gael@postsharp.net>
2022-11-17 16:14:08 +0100

    Fixed warnings.

--- Metalama.Framework.Engine/Introspection/AggregatedIntrospectionAspectLayer.cs
+++ Metalama.Framework.Engine/Introspection/AggregatedIntrospectionAspectLayer.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
-using Metalama.Framework.Engine.AspectOrdering;
-using Metalama.Framework.Engine.Utilities;
 using Metalama.Framework.Introspection;
-using System.Collections.Immutable;
 
 namespace Metalama.Framework.Engine.Introspection;
 
--- Metalama.Framework.Engine/Introspection/IntrospectionCompilationResultModel.cs
+++ Metalama.Framework.Engine/Introspection/IntrospectionCompilationResultModel.cs
@@ -1,6 +1,5 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
-using Metalama.Framework.Aspects;
 using Metalama.Framework.Code;
 using Metalama.Framework.Diagnostics;
 using Metalama.Framework.Engine.Aspects;
@@ -8,7 +7,6 @@ using Metalama.Framework.Engine.CodeModel;
 using Metalama.Framework.Engine.Pipeline;
 using Metalama.Framework.Engine.Utilities;
 using Metalama.Framework.Introspection;
-using System;
 using System.Collections.Immutable;
 using System.Linq;
 
