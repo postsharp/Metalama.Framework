7d82abe313c7d49782f1c433a81a69f7a55eb2ce
GaelFraiteur <gael@postsharp.net>
2022-11-24 19:14:51 +0100

    #32292 Version mismatches of System.Collections.Immutable on both ends of the pipe

---
+++ tests/Metalama.Framework.Tests.UnitTests.Internals/Remoting/SerializationBinderTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Framework.DesignTime.Rpc;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Xunit;
+
+namespace Metalama.Framework.Tests.UnitTests.Remoting;
+
+public class SerializationBinderTests
+{
+    [Theory]
+    [InlineData( typeof(int) )]
+    [InlineData( typeof(ProjectKey) )]
+    [InlineData( typeof(ImmutableArray<string>) )]
+    [InlineData( typeof(ImmutableArray<ProjectKey>) )]
+    public void Binder( Type type )
+    {
+        var binder = new JsonSerializationBinder();
+        binder.BindToName( type, out var assemblyName, out var typeName );
+        assemblyName = JsonSerializationBinder.RemoveAssemblyDetailsFromAssemblyName( assemblyName! );
+        typeName = JsonSerializationBinder.RemoveAssemblyDetailsFromTypeName( typeName! );
+        var roundloopType = binder.BindToType( assemblyName, typeName );
+        Assert.Same( type, roundloopType );
+    }
+
+    [Theory]
+    [InlineData(
+        typeof(ImmutableArray<ProjectKey>),
+        "System.Collections.Immutable.ImmutableArray`1[[Metalama.Framework.DesignTime.Rpc.ProjectKey, Metalama.Framework.DesignTime.Rpc, VERSION]]" )]
+    [InlineData(
+        typeof(ImmutableDictionary<string, string>),
+        "System.Collections.Immutable.ImmutableDictionary`2[[System.String, System.Private.CoreLib, VERSION],[System.String, System.Private.CoreLib, VERSION]]" )]
+    public void QualifyTypeName( Type type, string expectedQualifiedName )
+    {
+        var binder = new JsonSerializationBinder();
+        binder.BindToName( type, out _, out var typeName );
+        typeName = JsonSerializationBinder.RemoveAssemblyDetailsFromTypeName( typeName! );
+
+        var qualified = JsonSerializationBinder.QualifyAssemblies(
+            typeName,
+            new Dictionary<string, string>()
+            {
+                { "Metalama.Framework.DesignTime.Rpc", "Metalama.Framework.DesignTime.Rpc, VERSION" },
+                { "System.Collections.Immutable", "System.Collections.Immutable, VERSION" },
+                { "System.Private.CoreLib", "System.Private.CoreLib, VERSION" }
+
+                // TODO: System.Collections.Immutable.ImmutableDictionary`2[[System.String, System.Private.CoreLib],[System.String, System.Private.CoreLib]]
+            } );
+
+        Assert.Equal( expectedQualifiedName, qualified );
+    }
+}
