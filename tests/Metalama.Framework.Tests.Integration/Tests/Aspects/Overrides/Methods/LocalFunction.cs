5bd001ae9b72fa00d57738eb2ef5fc2376005ae3
Daniel Balas <daniel@postsharp.net>
2022-12-13 12:34:34 +0100

    #28055 Test with local function in the template.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Overrides/Methods/LocalFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Overrides.Methods.LocalFunction
+{
+    public class OverrideAttribute : OverrideMethodAspect
+    {
+        public override dynamic OverrideMethod()
+        {
+            Console.WriteLine("This is overridden method.");
+
+            return Quz();
+
+            int Quz()
+            {
+                var x = meta.Proceed();
+                return x + 1;
+            }
+        }
+    }
+
+    // <target>
+    internal class TargetClass 
+    {
+        [Override]
+        public int Foo(int x)
+        {
+            return Bar(Bar(x));
+
+            int Bar( int x )
+            {
+                return x + 1;
+            }
+        }
+    }
+}
