99eaa3bfc7f5bf17d1a72cd8203861b19cfbbd6c
GaelFraiteur <gael@postsharp.net>
2023-01-22 12:51:15 +0100

    Fixes for Metalama.Try.

--- Metalama.Framework.Engine/Pipeline/ICompileTimeExceptionHandler.cs
+++ Metalama.Framework.Engine/Pipeline/ICompileTimeExceptionHandler.cs
@@ -1,12 +1,14 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
+using JetBrains.Annotations;
 using Metalama.Framework.Services;
 using Microsoft.CodeAnalysis;
 using System;
 
 namespace Metalama.Framework.Engine.Pipeline
 {
-    internal interface ICompileTimeExceptionHandler : IGlobalService
+    [PublicAPI] // Implemented by Metalama.Try.
+    public interface ICompileTimeExceptionHandler : IGlobalService
     {
         void ReportException( Exception exception, Action<Diagnostic> reportDiagnostic, bool canIgnoreException, out bool isHandled );
     }
