97a6522bee9a7f750b4a5d507d602a511eae5036
Petr Onderka <petro@postsharp.net>
2023-01-23 15:55:58 +0100

    #30040 Don't qualify introduced static fields

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Introductions/ReferencingStaticIntroductions.cs
@@ -0,0 +1,26 @@
+using Metalama.Framework.Aspects;
+using System.Threading;
+
+namespace Metalama.Framework.IntegrationTests.Aspects.Introductions.Fields.ReferencingStaticIntroductions;
+
+class IdAttribute : TypeAspect
+{
+    [Introduce]
+    private static int _nextId;
+
+    [Introduce]
+    public static int Id { get; } = Interlocked.Increment(ref _nextId);
+
+    [Introduce]
+    public static void Method(int? id)
+    {
+        if (id == null)
+        {
+            Method(Id);
+        }
+    }
+}
+
+// <target>
+[Id]
+internal class TargetClass { }
