3bed5bf4c6c4b590c9fa2d8415d726a978279104
Daniel Balas <daniel@postsharp.net>
2023-01-24 16:57:37 +0100

    #32562 Fix test outputs.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Introductions/Interfaces/ImplicitProperty_AccessorMismatch.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Introductions/Interfaces/ImplicitProperty_AccessorMismatch.t.cs
@@ -1,3 +1,3 @@
 // CompileTimeAspectPipeline.ExecuteAsync failed.
-// Error LAMA0517 on `TargetClass`: `The aspect 'Introduction' cannot implement an interface property 'IInterface.TemplateWithInit' in the type 'TargetClass' because the template 'IntroductionAttribute.TemplateWithInit' is missing 'set' accessor.`
-// Error LAMA0517 on `TargetClass`: `The aspect 'Introduction' cannot implement an interface property 'IInterface.TemplateWithoutInit' in the type 'TargetClass' because the template 'IntroductionAttribute.TemplateWithoutInit' is missing 'init' accessor.`
+// Error LAMA0517 on `TargetClass`: `The aspect 'Introduction' cannot implement an interface property 'IInterface.TemplateWithInit' in the type 'TargetClass' because the template 'IntroductionAttribute.TemplateWithInit' is missing a 'set' accessor.`
+// Error LAMA0517 on `TargetClass`: `The aspect 'Introduction' cannot implement an interface property 'IInterface.TemplateWithoutInit' in the type 'TargetClass' because the template 'IntroductionAttribute.TemplateWithoutInit' is missing a 'init' accessor.`
