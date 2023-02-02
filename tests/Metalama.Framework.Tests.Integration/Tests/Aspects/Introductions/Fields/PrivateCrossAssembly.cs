4c9878ce093e58fdec589899c18fe6f5f310d5cf
GaelFraiteur <gael@postsharp.net>
2023-02-02 18:05:38 +0100

    Adding more tests. Fixing #32701 Compilation failure when attempting to introduce a get-only automatic property when the aspect is in a different assembly

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Introductions/Fields/PrivateCrossAssembly.cs
@@ -0,0 +1,7 @@
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Introductions.Fields.PrivateCrossAssembly;
+
+[MyAspect]
+internal class C
+{
+    
+}
---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Introductions/Properties/PrivateCrossAssembly.cs
@@ -0,0 +1,7 @@
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Introductions.Properties.PrivateCrossAssembly;
+
+[MyAspect]
+internal class C
+{
+    
+}
