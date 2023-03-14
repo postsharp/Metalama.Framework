12ead4ded5c5c783aa7f9d14276b0c27eacafcf7
Petr Onderka <petro@postsharp.net>
2023-03-14 11:53:03 +0100

    #32717 Support incrementing dynamic values

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/IncrementInRuntimeConditionalBlock.t.cs
@@ -0,0 +1,19 @@
+[AutoIncrementAttribute]
+int Property
+{
+    get
+    {
+        if (oldValue != this._property)
+        {
+            this._property = this._property + 1;
+            this._property += 1;
+            this._property++;
+            ++this._property;
+        }
+        return this._property;
+    }
+    set
+    {
+        throw new global::System.NotImplementedException();
+    }
+}
