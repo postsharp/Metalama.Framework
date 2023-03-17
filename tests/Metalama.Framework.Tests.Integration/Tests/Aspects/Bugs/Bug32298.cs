9d141a0c9060c75ef896ad46121f70f329351664
Daniel Balas <daniel@postsharp.net>
2023-03-17 15:12:08 +0100

    Tests for cancelled bugs.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/Bug32298.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Diagnostics;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Bugs.Bug32298;
+
+public class OverrideAttribute : TypeAspect
+{
+    public override void BuildAspect(IAspectBuilder<INamedType> builder)
+    {
+        foreach (var field in builder.Target.ForCompilation(builder.Advice.MutableCompilation).Fields)
+        {
+            builder.Advice.Override(field, nameof(Template));
+        }
+    }
+
+    [Introduce]
+    public int IntroducedField;
+
+    [Template]
+    public dynamic? Template
+    {
+        get
+        {
+            Console.WriteLine("This is the overridden getter.");
+
+            return meta.Proceed();
+        }
+
+        set
+        {
+            Console.WriteLine("This is the overridden setter.");
+            meta.Proceed();
+        }
+    }
+}
+
+// <target>
+[Override]
+public class C
+{
+    void M() { }
+}
