51b87a2a9019d76cfb838128d56a4aa05d15fa90
Petr Onderka <petro@postsharp.net>
2023-03-27 16:44:40 +0200

    #32802 Improve null handling in a test

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Samples/DeepClone2.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Samples/DeepClone2.t.cs
@@ -6,7 +6,7 @@ internal class AutomaticallyCloneable : global::System.ICloneable
     private AutomaticallyCloneable? _c;
     public virtual global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.AutomaticallyCloneable Clone()
     {
-        var clone = ((global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.AutomaticallyCloneable)base.MemberwiseClone())!;
+        var clone = ((global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.AutomaticallyCloneable)base.MemberwiseClone());
         ((global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.AutomaticallyCloneable)clone)._b = ((global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.ManuallyCloneable?)this._b?.Clone()!);
         ((global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.AutomaticallyCloneable)clone)._c = ((global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.AutomaticallyCloneable?)this._c?.Clone()!);
         return (global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.AutomaticallyCloneable)clone;
@@ -21,7 +21,7 @@ internal class DerivedCloneable : AutomaticallyCloneable
     private string? _d;
     public override global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.DerivedCloneable Clone()
     {
-        var clone = ((global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.DerivedCloneable)base.Clone())!;
+        var clone = ((global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.DerivedCloneable)base.Clone());
         return (global::Metalama.Framework.Tests.Integration.Tests.Aspects.Samples.DeepClone2.DerivedCloneable)clone;
     }
 }
