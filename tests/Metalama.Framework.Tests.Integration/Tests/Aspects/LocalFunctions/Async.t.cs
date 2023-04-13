d1488174427aeda31e06321001ec5d6a65cccd61
GaelFraiteur <gael@postsharp.net>
2023-04-13 16:02:37 +0200

    Accepting tests.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/LocalFunctions/Async.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/LocalFunctions/Async.t.cs
@@ -1,27 +1,23 @@
 // Warning CS1998 on `FooAsync_Source`: `This async method lacks 'await' operators and will run synchronously. Consider using the 'await' operator to await non-blocking API calls, or 'await Task.Run(...)' to do CPU-bound work on a background thread.`
-
 internal class C
 {
-    [Retry]
-    private void Foo()
+  [Retry]
+  private void Foo()
+  {
+    return;
+  }
+  [Retry]
+  private async Task FooAsync()
+  {
+    async global::System.Threading.Tasks.Task<global::System.Object?> ExecuteCoreAsync()
     {
-        return;
+      await this.FooAsync_Source();
+      return default;
     }
-
-    [Retry]
-    private async Task FooAsync()
-    {
-        async global::System.Threading.Tasks.Task<global::System.Object?> ExecuteCoreAsync()
-        {
-            await this.FooAsync_Source();
-            return default;
-        }
-
-        await global::System.Threading.Tasks.Task.Run(ExecuteCoreAsync);
-        return;
-    }
-
-    private async Task FooAsync_Source()
-    {
-    }
+    await global::System.Threading.Tasks.Task.Run(ExecuteCoreAsync);
+    return;
+  }
+  private async Task FooAsync_Source()
+  {
+  }
 }
