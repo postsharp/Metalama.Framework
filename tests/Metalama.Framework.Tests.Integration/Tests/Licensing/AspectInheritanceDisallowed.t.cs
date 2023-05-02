be7b0ce6d92f818e9d80c0d1198b33ce45df09e8
Antonín Procházka <antonin@postsharp.net>
2023-05-02 17:04:47 +0200

    #33015 Test fixed.

--- tests/Metalama.Framework.Tests.Integration/Tests/Licensing/AspectInheritanceDisallowed.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Licensing/AspectInheritanceDisallowed.t.cs
@@ -1,10 +1,10 @@
 // --- AspectInheritanceDisallowed.cs ---
-// Warning LAMA0802 on ``: `The 'Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritance.InstantiatedInheritableAspect', 'Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritance.InstatiatedConditionallyInheritableAspect' aspects cannot be inherited because this feature is not covered by your license.`
+// Warning LAMA0802 on ``: `The 'Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritance.InstantiatedInheritableAspect', 'Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritance.InstatiatedConditionallyInheritableAspect' aspects shall not be automatically inherited because this feature is not covered by your license. Aspects must be explicitly added to all derived types using custom attributes.`
 namespace Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritanceDisallowed;
 class Dummy
 {
 } // --- _AspectInheritance.cs ---
-// Warning LAMA0802 on ``: `The 'Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritance.InstantiatedInheritableAspect', 'Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritance.InstatiatedConditionallyInheritableAspect' aspects cannot be inherited because this feature is not covered by your license.`
+// Warning LAMA0802 on ``: `The 'Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritance.InstantiatedInheritableAspect', 'Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritance.InstatiatedConditionallyInheritableAspect' aspects shall not be automatically inherited because this feature is not covered by your license. Aspects must be explicitly added to all derived types using custom attributes.`
 using  Metalama . Framework . Aspects ;  using  System ;
 namespace Metalama.Framework.Tests.Integration.Tests.Licensing.AspectInheritance;
 #pragma warning disable CS0067, CS8618, CS0162, CS0169, CS0414, CA1822, CA1823, IDE0051, IDE0052
