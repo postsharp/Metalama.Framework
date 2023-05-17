966cd3c407c3c8eea9b3ffa5cc3df956c21e6860
Daniel Balas <daniel@postsharp.net>
2023-05-17 12:24:08 +0200

    #33163 Linker: access modifiers are not removed from property accessors when creating "_Source" declaration.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/Bug33163.cs
@@ -0,0 +1,56 @@
+
+using System;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Engine.Advising;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Bugs.Bug33163;
+
+
+public sealed class TestAspectAttribute : OverrideFieldOrPropertyAspect
+{
+    public override dynamic? OverrideProperty 
+    {
+        get => 42;
+        set 
+        {
+            meta.Proceed();
+            meta.Proceed();
+        }
+    }
+}
+
+// <target>
+public class TestClass
+{
+    [TestAspect]
+    public int PrivateSetter 
+    {
+        get
+        {
+            return 42;
+        }
+        
+        private set
+        {
+        }
+    }
+
+    [TestAspect]
+    public int PrivateSetter_Auto { get; private set; }
+
+    [TestAspect]
+    public int PrivateGetter
+    {
+        private get
+        {
+            return 42;
+        }
+
+        set
+        {
+        }
+    }
+
+    [TestAspect]
+    public int PrivateGetter_Auto { private get; set; }
+}
