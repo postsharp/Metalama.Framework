181434499de16bab4649dd81d6ff16b43089856c
Petr Onderka <petro@postsharp.net>
2023-06-13 18:24:50 +0200

    #33003 Register user diagnostics from referenced libraries

--- tests/Metalama.Framework.Tests.UnitTests/DesignTime/UserProfile/UserDiagnosticRegistrationServiceTests.cs
+++ tests/Metalama.Framework.Tests.UnitTests/DesignTime/UserProfile/UserDiagnosticRegistrationServiceTests.cs
@@ -5,7 +5,10 @@ using Metalama.Framework.DesignTime.Diagnostics;
 using Metalama.Framework.Engine.Services;
 using Metalama.Framework.Tests.UnitTests.DesignTime.Mocks;
 using Metalama.Testing.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Xunit;
 
 namespace Metalama.Framework.Tests.UnitTests.DesignTime.UserProfile
@@ -64,6 +67,63 @@ namespace Metalama.Framework.Tests.UnitTests.DesignTime.TestCode
         }
 
         [Fact]
+        public void TestUserErrorFromDependency()
+        {
+            var log = new StringBuilder();
+
+            this.GetUserDiagnosticsFileContent(
+                dependentCode: """
+                using System;
+                using Metalama.Framework.Aspects;
+                using Metalama.Framework.Code;
+                using Metalama.Framework.Diagnostics;
+
+                namespace Metalama.Framework.Tests.UnitTests.DesignTime.TestCode;
+
+                public class ReportErrorAttribute : MethodAspect
+                {
+                    private static readonly DiagnosticDefinition<IMethod> _userError = new(
+                            "MY001",
+                            Severity.Error,
+                            "User error description.");
+
+                    public override void BuildAspect(IAspectBuilder<IMethod> builder)
+                    {
+                        builder.Diagnostics.Report( _userError.WithArguments( builder.Target ) );
+                    }
+                }
+                """,
+                targetCode: """
+                namespace Metalama.Framework.Tests.UnitTests.DesignTime.TestCode;
+
+                class TargetCode
+                {
+                    [ReportError]
+                    public void Foo() { }
+                }
+                """,
+                aspectCode: "",
+                configurationFileChanged: ( configurationManager, file ) =>
+                {
+                    if ( file is UserDiagnosticsConfiguration { Diagnostics.IsEmpty: false } userDiagnostics )
+                    {
+                        log.AppendLine( string.Join( ", ", userDiagnostics.Diagnostics.SelectAsEnumerable( d => d.Value.Id ) ) );
+
+                        configurationManager.Set( new UserDiagnosticsConfiguration() );
+                    }
+                } );
+
+            // Verify that the diagnostic is reported for both compilations.
+            Assert.Equal(
+                """
+                MY001
+                MY001
+
+                """,
+                log.ToString() );
+        }
+
+        [Fact]
         public void TestDiagnosticSuppression()
         {
             var output =
@@ -111,17 +171,18 @@ namespace Metalama.Framework.Tests.UnitTests.DesignTime.TestCode
             Assert.Single( output.Suppressions );
         }
 
-        private UserDiagnosticsConfiguration GetUserDiagnosticsFileContent( string aspectCode, string targetCode )
+        private UserDiagnosticsConfiguration GetUserDiagnosticsFileContent( string aspectCode, string targetCode, string? dependentCode = null, Action<InMemoryConfigurationManager, ConfigurationFile>? configurationFileChanged = null )
         {
             using var testContext = this.CreateTestContext();
 
             var code = new Dictionary<string, string> { ["Aspect.cs"] = aspectCode, ["Class1.cs"] = targetCode };
 
-            var compilation = TestCompilationFactory.CreateCSharpCompilation( code );
+            var compilation = TestCompilationFactory.CreateCSharpCompilation( code, dependentCode );
 
             // Create a service provider with our own configuration manager.
             var configurationManager = testContext.ServiceProvider.Global.GetRequiredBackstageService<IConfigurationManager>();
-            Assert.IsType<InMemoryConfigurationManager>( configurationManager );
+            var inMemoryConfigurationManager = Assert.IsType<InMemoryConfigurationManager>( configurationManager );
+            inMemoryConfigurationManager.ConfigurationFileChanged += file => configurationFileChanged?.Invoke( inMemoryConfigurationManager, file );
             var serviceProvider = testContext.ServiceProvider.Global.Underlying.WithUntypedService( typeof(IConfigurationManager), configurationManager );
 
             using var pipelineFactory = new TestDesignTimeAspectPipelineFactory( testContext, serviceProvider );
