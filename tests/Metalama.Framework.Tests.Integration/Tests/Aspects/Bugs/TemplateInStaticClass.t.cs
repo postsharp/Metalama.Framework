9ae89c3eb5ec538688138dfc48f51be39f79cd41
Petr Onderka <petro@postsharp.net>
2023-06-16 13:12:52 +0200

    Fixed error message in test

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/TemplateInStaticClass.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/TemplateInStaticClass.t.cs
@@ -1,2 +1,2 @@
 // CompileTimeAspectPipeline.ExecuteAsync failed.
-// Error LAMA0266 on `DoSomething`: `The template 'MethodBuilder.DoSomething()' is containined within the static type 'MethodBuilder', which is not useful and not supported.`
+// Error LAMA0266 on `DoSomething`: `The template 'MethodBuilder.DoSomething()' is contained within the static type 'MethodBuilder', which is not supported.`
