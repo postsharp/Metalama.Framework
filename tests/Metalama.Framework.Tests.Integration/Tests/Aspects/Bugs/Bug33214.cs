16e082700d95d0de06a386116835371ee75d9e2b
Daniel Balas <daniel@postsharp.net>
2023-06-28 11:13:41 +0200

    #33417 Revert the revert.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/Bug33214.cs
@@ -0,0 +1,43 @@
+
+using System;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Engine.Advising;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Bugs.Bug33214;
+
+[Inheritable]
+public sealed class TestContract : ContractAspect
+{
+    public override void Validate(dynamic? value)
+    {
+        Console.WriteLine("Should be applied only on Foo(int) parameter.");
+    }
+}
+
+[Inheritable]
+public sealed class TestOverride : OverrideMethodAspect
+{
+    public override dynamic OverrideMethod()
+    {
+        Console.WriteLine("Should be applied only on Bar(int) method.");
+        return meta.Proceed();
+    }
+}
+
+public interface TestInterface
+{
+    void Foo();
+    void Foo([TestContract] int value);
+    void Bar();
+    [TestOverride]
+    void Bar(int value);
+}
+
+// <target>
+public class TestClass : TestInterface
+{
+    public void Foo() { }
+    public void Foo(int value) { }
+    public void Bar() { }
+    public void Bar(int value) { }
+}
