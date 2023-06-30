942d76a55952cbddac41abc849a399cccd2b8d66
Petr Onderka <petro@postsharp.net>
2023-06-30 17:14:05 +0200

    #33420 Prevent exception for ambiguous references to IAsyncEnumerable<T>

---
+++ tests/Standalone/IAsyncEnumerableNetStandard/Aspect.cs
@@ -0,0 +1,33 @@
+﻿using Metalama.Framework.Advising;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+using System;
+
+class Aspect : OverrideMethodAspect
+{
+    public override void BuildAspect( IAspectBuilder<IMethod> builder )
+    {
+        base.BuildAspect( builder );
+
+        var templates = new MethodTemplateSelector(
+            nameof(this.OverrideMethod),
+            nameof(this.OverrideAsyncMethod),
+            nameof(this.OverrideEnumerableMethod),
+            nameof(this.OverrideEnumeratorMethod),
+            nameof(this.OverrideAsyncEnumerableMethod),
+            nameof(this.OverrideAsyncEnumeratorMethod) );
+
+        builder.Advice.Override( builder.Target, templates );
+    }
+
+    public override dynamic? OverrideMethod()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Target
+{
+    [Aspect]
+    static void M() { }
+}
