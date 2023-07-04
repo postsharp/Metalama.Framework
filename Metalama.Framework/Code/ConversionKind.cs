299a62fcf61511532627b4b434ea23053c05d026
Daniel Balaš <daniel@postsharp.net>
2023-07-04 09:02:14 +0000

    Apply suggestions from code review

--- Metalama.Framework/Code/ConversionKind.cs
+++ Metalama.Framework/Code/ConversionKind.cs
@@ -15,17 +15,17 @@ namespace Metalama.Framework.Code
         Default,
 
         /// <summary>
-        /// Accepts any value that is reference-compatible with the given type (unlike the C# <c>is</c> operator).
+        /// Accepts any value that is reference-compatible with the given type i.e. instances of subclasses or interface implementations, but refuses boxing conversions.
         /// </summary>
         Reference,
 
         /// <summary>
-        /// Accepts any value that is reference-compatible or can be boxed, including boxing (behavior of C# <c>is</c> operator).
+        /// Accepts any value that is type-compatible, including boxing conversions, but excluding user-defined implicit operators. This corresponds to the bbehavior of C# <c>is</c> operator.
         /// </summary>
         ReferenceOrBoxing,
 
         /// <summary>
-        /// Accepts any value, which extends or implements a type which is of the same type definition as the given type definition.
+        /// Accepts any value that extends or implements a type that is of the same type definition as the given type definition.
         /// </summary>
         /// <remarks>
         /// For non-generic types behaves like <see cref="Reference"/>. For generic types, ignores all type arguments and tests that 
