7d37f66d201abffe988c5c4c3cc9d30071389804
GaelFraiteur <gael@postsharp.net>
2023-07-14 17:43:22 +0200

    Fixed test.

--- tests/Metalama.Framework.Tests.Integration/Tests/Fabrics/InheritableTypeFabrics.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Fabrics/InheritableTypeFabrics.t.cs
@@ -1,22 +1,36 @@
-internal class TargetCode
+internal class BaseClass
 {
-  private int Method1(int a)
-  {
-    global::System.Console.WriteLine("overridden");
-    return a;
-  }
-  private string Method2(string s)
-  {
-    global::System.Console.WriteLine("overridden");
-    return s;
-  }
+    private int Method1(int a)
+    {
+        global::System.Console.WriteLine("overridden");
+        return a;
+    }
+    private string Method2(string s)
+    {
+        global::System.Console.WriteLine("overridden");
+        return s;
+    }
 #pragma warning disable CS0067, CS8618, CS0162, CS0169, CS0414, CA1822, CA1823, IDE0051, IDE0052
-  private class Fabric : TypeFabric
-  {
-    public override void AmendType(ITypeAmender amender) => throw new System.NotSupportedException("Compile-time-only code cannot be called at run-time.");
-    [Template]
-    [global::Metalama.Framework.Aspects.CompiledTemplateAttribute(Accessibility = global::Metalama.Framework.Code.Accessibility.Private, IsAsync = false, IsIteratorMethod = false)]
-    private dynamic? Template() => throw new System.NotSupportedException("Compile-time-only code cannot be called at run-time.");
-  }
+    [Inheritable]
+    private class Fabric : TypeFabric
+    {
+        public override void AmendType(ITypeAmender amender) => throw new System.NotSupportedException("Compile-time-only code cannot be called at run-time.");
+        [Template]
+        [global::Metalama.Framework.Aspects.CompiledTemplateAttribute(Accessibility = global::Metalama.Framework.Code.Accessibility.Private, IsAsync = false, IsIteratorMethod = false)]
+        private dynamic? Template() => throw new System.NotSupportedException("Compile-time-only code cannot be called at run-time.");
+    }
 #pragma warning restore CS0067, CS8618, CS0162, CS0169, CS0414, CA1822, CA1823, IDE0051, IDE0052
+}
+internal class DerivedClass : BaseClass
+{
+    private int Method3(int a)
+    {
+        global::System.Console.WriteLine("overridden");
+        return a;
+    }
+    private string Method4(string s)
+    {
+        global::System.Console.WriteLine("overridden");
+        return s;
+    }
 }
