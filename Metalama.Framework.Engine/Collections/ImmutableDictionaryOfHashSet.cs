c3e2c1814f3a7f36f835305093d45976052f97b3
GaelFraiteur <gael@postsharp.net>
2023-07-21 19:12:04 +0200

    Fixing warnings.

--- Metalama.Framework.Engine/Collections/ImmutableDictionaryOfHashSet.cs
+++ Metalama.Framework.Engine/Collections/ImmutableDictionaryOfHashSet.cs
@@ -1,7 +1,6 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using JetBrains.Annotations;
-using Metalama.Framework.Engine.CodeModel;
 using System;
 using System.Collections;
 using System.Collections.Generic;
--- Metalama.Framework/Diagnostics/IDiagnosticSource.cs
+++ Metalama.Framework/Diagnostics/IDiagnosticSource.cs
@@ -1,4 +1,6 @@
-﻿using Metalama.Framework.Aspects;
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Framework.Aspects;
 
 namespace Metalama.Framework.Diagnostics;
 
