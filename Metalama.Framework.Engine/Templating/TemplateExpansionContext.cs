b383176dab727603d4216b15a230756165b053f6
Daniel Balas <daniel@postsharp.net>
2023-08-28 17:34:06 +0200

    #33685 Jetbrains annotations are not needed.

--- Metalama.Framework.Engine/Templating/TemplateExpansionContext.cs
+++ Metalama.Framework.Engine/Templating/TemplateExpansionContext.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
-using JetBrains.Annotations;
 using Metalama.Framework.Code;
 using Metalama.Framework.CompileTimeContracts;
 using Metalama.Framework.Engine.Advising;
