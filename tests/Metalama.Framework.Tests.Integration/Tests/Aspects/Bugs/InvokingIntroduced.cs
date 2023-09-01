63ff589c217688c0f1ce96c98ed832af7f5b14d1
Petr Onderka <petro@postsharp.net>
2023-09-01 17:19:24 +0200

    #33669 Fix invoking introduced interface member

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/InvokingIntroduced.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Bugs.InvokingIntroduced;
+
+class IntroduceAndInvokeAttribute : TypeAspect
+{
+    public override void BuildAspect(IAspectBuilder<INamedType> builder)
+    {
+        base.BuildAspect(builder);
+
+        var introducedMember = builder.Advice.IntroduceMethod(builder.Target, nameof(Introduce));
+        builder.Advice.IntroduceMethod(builder.Target, nameof(Invoke), buildMethod: method => method.Name = "Invoke0", args: new { introduced = introducedMember.Declaration });
+
+        var interfaceImplementation = builder.Advice.ImplementInterface(builder.Target, typeof(IFoo));
+        builder.Advice.IntroduceMethod(builder.Target, nameof(Invoke), args: new { introduced = interfaceImplementation.InterfaceMembers.Single().TargetMember });
+    }
+
+    [InterfaceMember]
+    public void Bar() { }
+
+    [Template]
+    public void Introduce() { }
+
+    [Template]
+    public void Invoke(IMethod introduced)
+    {
+        introduced.Invoke();
+    }
+}
+
+internal interface IFoo
+{
+    void Bar();
+}
+
+// <target>
+[IntroduceAndInvoke]
+class Target
+{
+}
---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/InvokingIntroduced.t.cs
@@ -0,0 +1,18 @@
+[IntroduceAndInvoke]
+class Target : global::Metalama.Framework.Tests.Integration.Tests.Aspects.Bugs.InvokingIntroduced.IFoo
+{
+    public void Bar()
+    {
+    }
+    public void Introduce()
+    {
+    }
+    public void Invoke()
+    {
+        this.Bar();
+    }
+    public void Invoke0()
+    {
+        this.Introduce();
+    }
+}
