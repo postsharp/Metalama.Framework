92d0026bac469adbddfbdccb2fbaaac0e4433ffe
GaelFraiteur <gael@postsharp.net>
2023-09-09 18:44:29 +0200

    Warnings.

--- Metalama.Testing.AspectTesting/XunitFramework/TestOutputHelper.cs
+++ Metalama.Testing.AspectTesting/XunitFramework/TestOutputHelper.cs
@@ -1,7 +1,7 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using System;
-using System.IO;
+using System.Globalization;
 using System.Text;
 using Xunit.Abstractions;
 using Xunit.Sdk;
@@ -27,7 +27,7 @@ namespace Metalama.Testing.AspectTesting.XunitFramework
             this._stringBuilder.Append( line );
         }
 
-        public void WriteLine( string format, params object[] args ) => this.WriteLine( string.Format( format, args ) );
+        public void WriteLine( string format, params object[] args ) => this.WriteLine( string.Format( CultureInfo.InvariantCulture, format, args ) );
 
         public override string ToString() => this._stringBuilder.ToString();
     }
