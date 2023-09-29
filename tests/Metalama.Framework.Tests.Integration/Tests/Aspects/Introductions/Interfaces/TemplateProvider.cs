11f2b52b43299169af876fa69c28bc476aa50ccb
Daniel Balas <daniel@postsharp.net>
2023-09-29 14:43:58 +0200

    #33870 Advice.WithTemplateProvider().ImplementInterface does not find correct [InterfaceMember].

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Introductions/Interfaces/TemplateProvider.cs
@@ -0,0 +1,48 @@
+﻿using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+using System;
+
+#pragma warning disable CS0067
+
+namespace Metalama.Framework.Tests.Integration.TestInputs.Aspects.Introductions.Interfaces.TemplateProvider
+{
+    /*
+     * Tests a simple case with implicit interface member defined in a template provider.
+     */
+
+    public interface IInterface
+    {
+        void Foo();
+        void Bar();
+    }
+
+    public class IntroductionAttribute : TypeAspect
+    {
+        public override void BuildAspect( IAspectBuilder<INamedType> aspectBuilder )
+        {
+            aspectBuilder.Advice.WithTemplateProvider(new TemplateProvider()).ImplementInterface( aspectBuilder.Target, typeof(IInterface) );
+        }
+    }
+
+    public class TemplateProviderBase : ITemplateProvider
+    {
+        [InterfaceMember]
+        public void Foo()
+        {
+            Console.WriteLine("Introduced interface member");
+        }
+    }
+
+    public class TemplateProvider : TemplateProviderBase
+    { 
+        [InterfaceMember]
+        public void Bar()
+        {
+            Console.WriteLine("Introduced interface member");
+        }
+    }
+
+    // <target>
+    [Introduction]
+    public class TargetClass { }
+}
