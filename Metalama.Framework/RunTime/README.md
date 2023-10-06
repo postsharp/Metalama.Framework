ba305d2d4b19381e64e2653928df63599aa4fa8b
Petr Onderka <petro@postsharp.net>
2023-10-06 18:25:25 +0200

    #33857 Moved Metalama.Framework.RunTime to separate repo

--- Metalama.Framework/RunTime/README.md
+++
@@ -1,3 +0,0 @@
-This namespace contains types that can be used at run time only. Other namespaces are compile-time only.
-
-We should probably split these in two projects.
--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -53,9 +53,15 @@ var product = new Product( MetalamaDependencies.Metalama )
         "Metalama.Framework.Introspection.$(PackageVersion).nupkg",
         "Metalama.Framework.Workspaces.$(PackageVersion).nupkg",
         "Metalama.Tool.$(PackageVersion).nupkg" ),
-    ParametrizedDependencies = new[] { DevelopmentDependencies.PostSharpEngineering.ToDependency(), MetalamaDependencies.MetalamaCompiler.ToDependency( new ConfigurationSpecific<BuildConfiguration>( 
-        BuildConfiguration.Release, BuildConfiguration.Release, BuildConfiguration.Public
-    )) },
+    ParametrizedDependencies = new[]
+    {
+        DevelopmentDependencies.PostSharpEngineering.ToDependency(),
+        MetalamaDependencies.MetalamaCompiler.ToDependency(
+            new ConfigurationSpecific<BuildConfiguration>(
+                BuildConfiguration.Release, BuildConfiguration.Release, BuildConfiguration.Public
+            ) ),
+        MetalamaDependencies.MetalamaFrameworkRunTime.ToDependency()
+    },
     Configurations = Product.DefaultConfigurations
         .WithValue( 
             BuildConfiguration.Debug,
