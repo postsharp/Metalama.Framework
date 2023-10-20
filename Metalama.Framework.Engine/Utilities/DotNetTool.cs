9b042ee9a1715d5f794b851bd7ca04c3b9630f3e
Petr Onderka <petro@postsharp.net>
2023-10-20 18:47:41 +0200

    #34032 Work around Rider PATH issue

--- Metalama.Framework.Engine/Utilities/DotNetTool.cs
+++ Metalama.Framework.Engine/Utilities/DotNetTool.cs
@@ -6,6 +6,8 @@ using Metalama.Framework.Engine.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Metalama.Framework.Engine.Utilities;
 
@@ -13,10 +15,32 @@ namespace Metalama.Framework.Engine.Utilities;
 public sealed class DotNetTool
 {
     private readonly IPlatformInfo _platformInfo;
+    private readonly string _path;
 
     public DotNetTool( GlobalServiceProvider serviceProvider )
     {
         this._platformInfo = serviceProvider.GetRequiredBackstageService<IPlatformInfo>();
+
+        this._path = Environment.GetEnvironmentVariable( "PATH" ) ?? string.Empty;
+        const string riderPath = "/lib/ReSharperHost/";
+        
+        if ( RuntimeInformation.IsOSPlatform( OSPlatform.Linux ) && this._path.ContainsOrdinal( riderPath ) )
+        {
+            var logger = serviceProvider.GetLoggerFactory().GetLogger( "DotNetTool" );
+
+            // When Rider is installed on Linux, it adds its own dotnet with no SDKs to its PATH.
+            // This breaks our dotnet commands, so we remove any paths related to Rider from the PATH for our tools.
+            // The path can look like the following, depending on whether Rider was installed using snap or the JetBrains Toolbox:
+            // /snap/rider/408/lib/ReSharperHost/linux-x64/dotnet/dotnet
+            // /home/petro/.local/share/JetBrains/Toolbox/apps/rider/lib/ReSharperHost/linux-x64/dotnet/dotnet
+
+            var paths = this._path.Split( ':' ).Where( path => !path.ContainsOrdinal( riderPath ) );
+            var newPath = string.Join( ":", paths );
+
+            logger.Info?.Log( $"Changing PATH from '{this._path}' to '{newPath}'." );
+
+            this._path = newPath;
+        }
     }
 
     public void Execute( string arguments, string? workingDirectory = null, int timeout = 30_000 )
@@ -32,7 +56,7 @@ public sealed class DotNetTool
 
             // We must avoid passing the following environment variables to the child process, otherwise there can be a mismatch
             // between SDK versions and the build will fail.
-            Environment = { { "DOTNET_ROOT_X64", null }, { "MSBUILD_EXE_PATH", null }, { "MSBuildSDKsPath", null } }
+            Environment = { { "DOTNET_ROOT_X64", null }, { "MSBUILD_EXE_PATH", null }, { "MSBuildSDKsPath", null }, { "PATH", this._path } }
         };
 
         var process = new Process() { StartInfo = startInfo };
