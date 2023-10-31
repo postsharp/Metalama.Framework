fea73492255e7a9f79245672a4faac7a541bbf26
Petr Onderka <petro@postsharp.net>
2023-10-31 18:25:25 +0100

    #33966 Added tests for C# 12 inline arrays

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/CSharp12/InlineArrays_CompileTime.t.cs
@@ -0,0 +1,5 @@
+// CompileTimeAspectPipeline.ExecuteAsync failed.
+// Error CS0021 on `buffer[i]`: `Cannot apply indexing with [] to an expression of type 'Buffer'`
+// Error CS1579 on `buffer`: `foreach statement cannot operate on variables of type 'Buffer' because 'Buffer' does not contain a public instance or extension definition for 'GetEnumerator'`
+// Error CS0021 on `buffer2[i]`: `Cannot apply indexing with [] to an expression of type 'Buffer'`
+// Error CS1579 on `buffer2`: `foreach statement cannot operate on variables of type 'Buffer' because 'Buffer' does not contain a public instance or extension definition for 'GetEnumerator'`
---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/CSharp12/InlineArrays_RunTime.cs
@@ -0,0 +1,64 @@
+#if TEST_OPTIONS
+// @RequiredConstant(NET8_0_OR_GREATER)
+#endif
+
+#if NET8_0_OR_GREATER
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.Tests.Integration.Tests.Aspects.CSharp12.InlineArrays_RunTime;
+
+public class TheAspect : OverrideMethodAspect
+{
+    public override void BuildAspect(IAspectBuilder<IMethod> builder)
+    {
+        base.BuildAspect(builder);
+    }
+
+    public override dynamic? OverrideMethod()
+    {
+        var buffer = new Buffer();
+        for (int i = 0; i < 10; i++)
+        {
+            buffer[i] = i;
+        }
+
+        foreach (var i in buffer)
+        {
+            Console.WriteLine(i);
+        }
+
+        return meta.Proceed();
+    }
+}
+
+[RunTimeOrCompileTime]
+#pragma warning disable CS0436 // Type conflicts with imported type
+[InlineArray(10)]
+#pragma warning restore CS0436
+public struct Buffer
+{
+    private int _element0;
+}
+
+public class C
+{
+    [TheAspect]
+    void M()
+    {
+        var buffer = new Buffer();
+        for (int i = 0; i < 10; i++)
+        {
+            buffer[i] = i;
+        }
+
+        foreach (var i in buffer)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+#endif
