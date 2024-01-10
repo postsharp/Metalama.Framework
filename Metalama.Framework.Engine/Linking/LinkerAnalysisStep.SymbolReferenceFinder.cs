eb07bb3bcbf6c2faf71da753d67b3c52469edbb8
Daniel Balas <daniel@postsharp.net>
2024-01-10 23:03:33 +0100

    Fix warning.

--- Metalama.Framework.Engine/Linking/LinkerAnalysisStep.SymbolReferenceFinder.cs
+++ Metalama.Framework.Engine/Linking/LinkerAnalysisStep.SymbolReferenceFinder.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using Metalama.Framework.Engine.Services;
-using Metalama.Framework.Engine.Utilities.Roslyn;
 using Metalama.Framework.Engine.Utilities.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
