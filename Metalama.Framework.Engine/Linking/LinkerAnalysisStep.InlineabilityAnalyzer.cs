6ae5239327c65f8f5aa986de51ff186beba8a497
Daniel Balas <daniel@postsharp.net>
2024-02-20 11:18:15 +0100

    Missing change.

--- Metalama.Framework.Engine/Linking/LinkerAnalysisStep.InlineabilityAnalyzer.cs
+++ Metalama.Framework.Engine/Linking/LinkerAnalysisStep.InlineabilityAnalyzer.cs
@@ -11,7 +11,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
