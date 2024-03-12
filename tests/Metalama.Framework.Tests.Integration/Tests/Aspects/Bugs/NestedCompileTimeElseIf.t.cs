fd08a144216c8f6940c3cf37bbfd2b2346e73b6a
Petr Onderka <petro@postsharp.net>
2024-03-12 12:10:24 +0100

    #33920 Fix handling of compile-time if-else chains

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/NestedCompileTimeElseIf.t.cs
@@ -0,0 +1,17 @@
+[Aspect(I = -1)]
+class TargetM1
+{
+    private void M()
+    {
+        global::System.Console.WriteLine(-1);
+        global::System.Console.WriteLine("I <= 0");
+    }
+}
+[Aspect(I = 1)]
+class Target1
+{
+    private void M()
+    {
+        global::System.Console.WriteLine(1);
+    }
+}
