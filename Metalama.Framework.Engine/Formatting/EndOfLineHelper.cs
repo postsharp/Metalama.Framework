04e7aec02b2029feff6f74d0c20fa6f6e2312f82
GaelFraiteur <gael@postsharp.net>
2024-03-21 14:44:57 +0100

    Removing previous EOL processing.

--- Metalama.Framework.Engine/Formatting/EndOfLineHelper.cs
+++ Metalama.Framework.Engine/Formatting/EndOfLineHelper.cs
@@ -10,22 +10,6 @@ namespace Metalama.Framework.Engine.Formatting
 {
     internal partial class EndOfLineHelper
     {
-        public static CompilationUnitSyntax NormalizeEndOfLineStyle( CompilationUnitSyntax compilationUnit )
-        {
-            DetermineEndOfLineStyle( compilationUnit, out var dominantStyle, out var isMixed );
-
-            if ( !isMixed )
-            {
-                return compilationUnit;
-            }
-            else
-            {
-                var rewriter = new TriviaRewriter( dominantStyle );
-
-                return (CompilationUnitSyntax) rewriter.Visit( compilationUnit ).AssertNotNull();
-            }
-        }
-
         /// <summary>
         /// Returns the first EOL string without allocating memory.
         /// </summary>
@@ -104,41 +88,6 @@ namespace Metalama.Framework.Engine.Formatting
             }
 
             return null;
-        }
-
-        private static void DetermineEndOfLineStyle( CompilationUnitSyntax compilationUnit, out EndOfLineStyle endOfLineStyle, out bool isMixed )
-        {
-            var visitor = new TriviaWalker();
-
-            visitor.Visit( compilationUnit );
-
-            isMixed = visitor.IsMixed;
-            endOfLineStyle = visitor.EndOfLineStyle;
-        }
-
-        [MethodImpl( MethodImplOptions.AggressiveInlining )]
-        private static EndOfLineStyle GetEndOfLineStyle( in ReadOnlySpan<char> chars )
-        {
-            if ( chars.Length >= 1 )
-            {
-                if ( chars.EndsWith( "\r\n".AsSpan() ) )
-                {
-                    return EndOfLineStyle.Windows;
-                }
-                else
-                {
-                    return chars[^1] switch
-                    {
-                        '\n' => EndOfLineStyle.Unix,
-                        '\r' => EndOfLineStyle.MacOs,
-                        _ => EndOfLineStyle.Unknown
-                    };
-                }
-            }
-            else
-            {
-                return EndOfLineStyle.Unknown;
-            }
         }
     }
 }
