67a5d049e75e94955ebc1aced29e3f8b5486091f
Daniel Balas <daniel@postsharp.net>
2024-03-28 22:43:37 +0100

    Improving/adding tests.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Attributes/Trivia_DocComment.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Attributes/Trivia_DocComment.cs
@@ -4,44 +4,126 @@ using System.Linq;
 using Metalama.Framework.Aspects;
 using Metalama.Framework.Code;
 using Metalama.Framework.Code.DeclarationBuilders;
+using Metalama.Framework.Fabrics;
 
 #pragma warning disable CS0169
 
 namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Attributes.Trivia_DocComment;
 
-public sealed class RequiredAttribute : PropertyAspect
+public sealed class TestAspect : TypeAspect
 {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public string ErrorMessage { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-    public override void BuildAspect(IAspectBuilder<IProperty> builder)
+    public override void BuildAspect(IAspectBuilder<INamedType> builder)
     {
-        IReadOnlyCollection<IAttribute> requiredAttribute = builder.Target.Attributes.OfAttributeType(typeof(RequiredAttribute)).ToList();
+        builder.Advice.IntroduceAttribute(
+            builder.Target,
+            AttributeConstruction.Create(
+                ((INamedType)TypeFactory.GetType(typeof(TestAttribute))).Constructors.Single()),
+            OverrideStrategy.Override);
 
-        foreach (IAttribute attribute in requiredAttribute)
+        foreach (var property in builder.Target.FieldsAndProperties)
         {
             builder.Advice.IntroduceAttribute(
-                builder.Target,
+                property,
                 AttributeConstruction.Create(
-                    attribute.Type.Constructors.Single(), attribute.ConstructorArguments, attribute.NamedArguments.Append(new KeyValuePair<string, TypedConstant>(nameof(RequiredAttribute.ErrorMessage), TypedConstant.Create("REQUIRED"))).ToList()),
+                    ((INamedType)TypeFactory.GetType(typeof(TestAttribute))).Constructors.Single() ),
                 OverrideStrategy.Override);
         }
     }
 }
 
-public class Range : Attribute
+public class ExistingAttribute : Attribute
 {
-    public Range(int start, int end)
+}
+
+public class TestAttribute : Attribute
+{
+}
+
+partial class TestTypes
+{
+    private class MyFabric : TypeFabric
     {
+        public override void AmendType(ITypeAmender amender)
+        {
+            amender.Outbound.SelectMany(t => t.NestedTypes).AddAspect<TestAspect>();
+        }
     }
 }
 
 // <target>
-class C
+partial class TestTypes
 {
-    /// <inheritdoc/>
-    [Required]
-    [Range(1, 1000)]
-    public int? PageSize { get; init; }
+    /// <summary>
+    /// </summary>
+    class C
+    {
+        /// <summary>
+        /// </summary>
+        public int? Field1;
+
+        /// <summary>
+        /// </summary>
+        [Test]
+        public int? Field2;
+
+        /// <summary>
+        /// </summary>
+        [Existing]
+        public int? Field3;
+
+        /// <summary>
+        /// </summary>
+        [Existing]
+        [Test]
+        public int? Field4;
+
+        /// <summary>
+        /// </summary>
+        public int? Property1 { get; }
+
+        /// <summary>
+        /// </summary>
+        [Test]
+        public int? Property2 { get; }
+
+        /// <summary>
+        /// </summary>
+        [Existing]
+        public int? Property3 { get; }
+
+        /// <summary>
+        /// </summary>
+        [Existing]
+        [Test]
+        public int? Property4 { get; }
+    }
+
+    /// <summary>
+    /// </summary>
+    enum E
+    {
+        /// <summary>
+        /// </summary>
+        Value1,
+
+        /// <summary>
+        /// </summary>
+        [Test]
+        Value2,
+
+        /// <summary>
+        /// </summary>
+        [Existing]
+        Value3,
+
+        /// <summary>
+        /// </summary>
+        [Existing]
+        [Test]
+        Value4,
+    }
 }
