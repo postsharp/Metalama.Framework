1cd82b582e167e80dbf7f91c1662c1c89e71df7f
GaelFraiteur <gael@postsharp.net>
2024-04-23 09:24:59 +0200

    Introspection: move internal interface.

--- Metalama.Framework.Engine/Introspection/IIntrospectionAspectPredecessorInternal.cs
+++ Metalama.Framework.Engine/Introspection/IIntrospectionAspectPredecessorInternal.cs
@@ -1,10 +1,11 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using Metalama.Framework.Aspects;
+using Metalama.Framework.Introspection;
 
-namespace Metalama.Framework.Introspection;
+namespace Metalama.Framework.Engine.Introspection;
 
-public interface IIntrospectionAspectPredecessorInternal : IIntrospectionAspectPredecessor
+internal interface IIntrospectionAspectPredecessorInternal : IIntrospectionAspectPredecessor
 {
     void AddSuccessor( AspectPredecessor aspectInstance );
 }
