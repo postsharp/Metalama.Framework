4434430ebac1f0b15ffdb9c9fbb0bdb7d7973aa5
GaelFraiteur <gael@postsharp.net>
2024-04-29 15:55:32 +0200

    Acknowledging issue.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Attributes/Params.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Attributes/Params.cs
@@ -6,18 +6,33 @@ using Metalama.Framework.Code.DeclarationBuilders;
 
 namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Attributes.Params;
 
+[AttributeUsage( AttributeTargets.All, AllowMultiple = true )]
 public class MyAttribute : Attribute
 {
     public MyAttribute( string a, params int[] p ) { }
 }
+
+[AttributeUsage( AttributeTargets.All, AllowMultiple = true )]
+public class YourAttribute : Attribute
+{
+    public YourAttribute( string a, params string?[] p ) { }
+}
+
 
 [Inheritable]
 public class MyAspect : MethodAspect
 {
     public override void BuildAspect( IAspectBuilder<IMethod> builder )
     {
-        var attr = AttributeConstruction.Create( typeof(MyAttribute), new object[] { "x", 1, 2, 3, 4, 5 } );
-        builder.Advice.IntroduceAttribute( builder.Target, attr );
+        var attr1 = AttributeConstruction.Create( typeof(MyAttribute), new object[] { "x", 1, 2, 3, 4, 5 } );
+        builder.Advice.IntroduceAttribute( builder.Target, attr1 );
+        
+        //  Known issue: should pass the 'null' argument, but does not.
+        var attr2 = AttributeConstruction.Create( typeof(MyAttribute), new object[] { "x", null } );
+        builder.Advice.IntroduceAttribute( builder.Target, attr2, whenExists: OverrideStrategy.New  );
+        
+        var attr3 = AttributeConstruction.Create( typeof(YourAttribute), new object[] { "x", null } );
+        builder.Advice.IntroduceAttribute( builder.Target, attr3, whenExists: OverrideStrategy.New );
     }
 }
 
