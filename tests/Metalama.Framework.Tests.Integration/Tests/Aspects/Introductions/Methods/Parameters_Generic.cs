ed770dc2eafc2ed25c4079e7216b7b359a57cace
Daniel Balas <daniel@postsharp.net>
2024-04-30 10:55:46 +0200

    Minor test.

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Introductions/Methods/Parameters_Generic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.IntegrationTests.Aspects.Introductions.Methods.Parameters_Generic
+{
+    public class IntroductionAttribute : TypeAspect
+    {
+        public override void BuildAspect( IAspectBuilder<INamedType> builder )
+        {
+            builder.Advice.IntroduceMethod(
+                builder.Target,
+                nameof(Template),
+                buildMethod: introduced =>
+                {
+                    introduced.Name = "IntroducedMethod";
+                    introduced.AddParameter( "x", builder.Target );
+                } );
+
+            // TODO: Other members.
+        }
+
+        [Template]
+        public dynamic? Template()
+        {
+            Console.WriteLine( "This is introduced method." );
+
+            return meta.Proceed();
+        }
+    }
+
+    // <target>
+    [Introduction]
+    internal class TargetClass<T> { }
+}
