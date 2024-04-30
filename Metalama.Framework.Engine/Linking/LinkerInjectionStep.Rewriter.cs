76870ea5a438ff2691b6c989008b6ddfcf9ed819
Daniel Balas <daniel@postsharp.net>
2024-04-30 11:37:29 +0200

    #34205 Removed fake symbols.

--- Metalama.Framework.Engine/Linking/LinkerInjectionStep.Rewriter.cs
+++ Metalama.Framework.Engine/Linking/LinkerInjectionStep.Rewriter.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using Metalama.Framework.Code;
-using Metalama.Framework.Code.DeclarationBuilders;
 using Metalama.Framework.Engine.CodeModel;
 using Metalama.Framework.Engine.CodeModel.Builders;
 using Metalama.Framework.Engine.CodeModel.References;
