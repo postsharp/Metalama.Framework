f3d7e6447e2f429bb27cef26d0647dc770a35c53
GaelFraiteur <gael@postsharp.net>
2024-05-03 17:26:05 +0200

    Accept test outputs.

--- tests/Metalama.Framework.Tests.Integration/Tests/Templating/Syntax/Lambdas/DynamicInLambda2.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Templating/Syntax/Lambdas/DynamicInLambda2.t.cs
@@ -1,3 +1,3 @@
 // TestTemplateCompiler.TryCompile failed.
-// Error LAMA0263 on `p => p.Value?.ToString()`: `Lambdas or anonymous functions returning a dynamic type are not supported except. Consider casting the result to IExpression.`
+// Error LAMA0263 on `p => p.Value?.ToString()`: `Lambdas or anonymous functions returning a dynamic type are not supported except. Consider casting the result to IExpression.  For void expressions, use a lambda statement.`
 // Error LAMA0104 on `p`: `The expression 'p' is run-time but it is expected to be compile-time because the expression appears in a compile-time-only member 'Value'.`
