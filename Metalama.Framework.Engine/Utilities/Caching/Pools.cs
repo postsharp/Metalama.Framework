b43d7756aaef7681272e8b6e40677537d0c8d3d2
GaelFraiteur <gael@postsharp.net>
2024-05-07 17:28:35 +0200

    Adding benchmarking program.

--- Metalama.Framework.Engine/Utilities/Caching/Pools.cs
+++ Metalama.Framework.Engine/Utilities/Caching/Pools.cs
@@ -6,5 +6,11 @@ namespace Metalama.Framework.Engine.Utilities.Caching;
 
 internal static class Pools
 {
+    /*
+        | Method    | Mean     | Error    | StdDev   | Median   |
+        |---------- |---------:|---------:|---------:|---------:|
+        | NewObject | 51.00 ns | 1.712 ns | 5.047 ns | 49.15 ns |
+        | Pooled    | 43.91 ns | 0.908 ns | 2.277 ns | 43.48 ns |
+     */
     public static ObjectPool<SemaphoreSlim> SemaphoreSlim { get; } = new( () => new SemaphoreSlim( 1 ) );
 }
