148ffe66d24dfc9b7f263350fb507f34bc6669f2
GaelFraiteur <gael@postsharp.net>
2024-05-19 21:22:33 +0200

    Fixed Build.ps1.

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,8 @@
+if ( $IsWindows -and $env:VisualStudioVersion -eq $null ) {
+    Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+    Enter-VsDevShell -VsInstallPath "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\" -StartInPath $(Get-Location)
+}
+
 (& dotnet nuget locals http-cache -c) | Out-Null
 & dotnet run --project "$PSScriptRoot/eng/src/BuildMetalama.csproj" -- $args
 exit $LASTEXITCODE
