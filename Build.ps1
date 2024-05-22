020034d875ff8707c6d32018f6237e4bc0f237e9
GaelFraiteur <gael@postsharp.net>
2024-05-22 14:06:16 +0200

    Docker: attempt to switch to Windows containers

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,4 @@
+& $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchWindowsEngine
 (& dotnet nuget locals http-cache -c) | Out-Null
 & dotnet run --project "$PSScriptRoot/eng/src/BuildMetalama.csproj" -- $args
 exit $LASTEXITCODE
