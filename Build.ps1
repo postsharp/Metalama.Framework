0bc5b2533054cfd7a3f0074c5e2ea9b1724dc65a
GaelFraiteur <gael@postsharp.net>
2024-05-23 12:37:12 +0200

    Fixed Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,4 +1,4 @@
 (& dotnet nuget locals http-cache -c) | Out-Null
-& dotnet run --project "$PSScriptRoot/eng/src/BuildMetalama.csproj" -- $args
+Start-Process -WorkingDirectory  "$PSScriptRoot/eng/src" -NoNewWindow -Wait -FilePath dotnet -ArgumentList "run $args" 
 exit $LASTEXITCODE
 
