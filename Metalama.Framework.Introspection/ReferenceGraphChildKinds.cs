77a5616e45bad866fbd81d87a76066057e9d467b
Daniel Balas <daniel@postsharp.net>
2024-06-26 16:55:57 +0200

    Zero warnings again.

--- Metalama.Framework.Introspection/ReferenceGraphChildKinds.cs
+++ Metalama.Framework.Introspection/ReferenceGraphChildKinds.cs
@@ -1,3 +1,5 @@
+// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
 namespace Metalama.Framework.Introspection;
 
 public enum ReferenceGraphChildKinds
--- Metalama.Framework.Workspaces/WorkspaceLoadException.cs
+++ Metalama.Framework.Workspaces/WorkspaceLoadException.cs
@@ -1,10 +1,12 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
+using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
 
 namespace Metalama.Framework.Workspaces;
 
+[PublicAPI]
 public sealed class WorkspaceLoadException : Exception
 {
     public IReadOnlyList<string> Errors { get; }
