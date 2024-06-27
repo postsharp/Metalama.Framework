d5178d33b655a0d71fe7175b02081d30b707504a
GaelFraiteur <gael@postsharp.net>
2024-06-27 17:26:19 +0200

    Fixing preview issue.

---
+++ Metalama.Framework.Workspaces/UserDiagnostic.cs
@@ -0,0 +1,88 @@
+﻿using Metalama.Framework.Code;
+using Metalama.Framework.Diagnostics;
+using Metalama.Framework.Engine.Diagnostics;
+using Metalama.Framework.Introspection;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Metalama.Framework.Workspaces;
+
+internal class UserDiagnostic : IIntrospectionDiagnostic
+{
+    public UserDiagnostic( Severity severity, string id, string message, string? filePath, int? line, IDeclaration? declaration, object? details )
+    {
+        this.Id = id;
+        this.Message = message;
+        this.FilePath = filePath;
+        this.Line = line;
+        this.Declaration = declaration;
+        this.Severity = severity;
+        this.Details = details;
+    }
+
+    public ICompilation? Compilation => this.Declaration?.Compilation;
+
+    public string Id { get;  }
+
+    public string Message { get; }
+
+    public string? FilePath { get;  }
+
+    public int? Line { get; }
+
+    public IDeclaration? Declaration { get; }
+
+    public Severity Severity { get;}
+
+    public IntrospectionDiagnosticSource Source => IntrospectionDiagnosticSource.User;
+
+    public object? Details { get; }
+}
+public static class DiagnosticReporter
+{
+    public static int Warnings { get; private set; }
+
+    public static Action<IIntrospectionDiagnostic>? ReportAction { get; set; } = d => Console.WriteLine( d.FormatAsBuildDiagnostic() );
+
+    public static IEnumerable<IIntrospectionDiagnostic> Report(this IEnumerable<IIntrospectionReference> references, Severity severity, string id, string message)
+        => references
+                .SelectMany(r => r.Details)
+                .Select(r => new DiagnosticTarget( r.Source.GetDiagnosticLocation(), r.Reference.OriginDeclaration, r ) )
+                .GroupBy(r => r.Location?.GetLineSpan().StartLinePosition.Line ) // Report a single warning per line.
+                .Select(g =>
+                {
+                    var items = g.ToReadOnlyList();
+
+                    return new DiagnosticTarget( items[0].Location, items[0].Declaration, items.Select( i => i.Details ).ToArray() );
+                } )
+                .Report(severity, id, message);
+
+    public static IEnumerable<IIntrospectionDiagnostic> Report(this IEnumerable<IDeclaration> declarations, Severity severity,string id, string message)
+        => declarations
+            .Select(x => new DiagnosticTarget( x.GetDiagnosticLocation(), x, null))
+            .Report(severity, id, message);
+
+    record DiagnosticTarget( Location? Location, IDeclaration Declaration, object? Details );
+    private static IEnumerable<IIntrospectionDiagnostic> Report(this IEnumerable<DiagnosticTarget> targets, Severity severity, string id, string message)
+    {
+        foreach (var location in targets)
+        {
+            Warnings++;
+
+            var diagnostic = new UserDiagnostic(
+                severity,
+                id,
+                message,
+                location.Location.SourceTree?.FilePath,
+                location.Location.GetLineSpan().StartLinePosition.Line + 1,
+                location.Declaration,
+                location.Details );
+            
+            ReportAction?.Invoke( diagnostic );
+            
+            
+        }
+    }
+}
