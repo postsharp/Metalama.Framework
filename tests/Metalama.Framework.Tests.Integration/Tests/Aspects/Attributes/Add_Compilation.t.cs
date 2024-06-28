2a01e185bfdfec400d0ebc6de71c5f890a2c7cee
Petr Onderka <petro@postsharp.net>
2024-06-28 12:13:59 +0200

    #35134 Fix handling of assembly attributes

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Attributes/Add_Compilation.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Attributes/Add_Compilation.t.cs
@@ -5,7 +5,6 @@ using Metalama.Framework.Code;
 using Metalama.Framework.Code.DeclarationBuilders;
 using Metalama.Framework.Tests.Integration.Tests.Aspects.Attributes.Add_Compilation;
 [assembly: MyAspect]
-[assembly: global::Metalama.Framework.Tests.Integration.Tests.Aspects.Attributes.Add_Compilation.MyAttribute]
 namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Attributes.Add_Compilation;
 public class MyAttribute : Attribute
 {
