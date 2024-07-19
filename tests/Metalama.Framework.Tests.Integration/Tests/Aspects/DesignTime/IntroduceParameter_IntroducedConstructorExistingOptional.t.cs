f8d5d0dbf402f38852e8a536e6df3b1aaa6d39a9
Daniel Balas <daniel@postsharp.net>
2024-07-19 08:38:01 +0200

    Fix rest results.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/DesignTime/IntroduceParameter_IntroducedConstructorExistingOptional.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/DesignTime/IntroduceParameter_IntroducedConstructorExistingOptional.t.cs
@@ -8,7 +8,7 @@ namespace Metalama.Framework.IntegrationTests.Aspects.DesignTime.IntroduceParame
     public TestClass(global::System.Int32 param, global::System.Int32 optParam = 42, global::System.Int32 introduced1 = 42, global::System.String introduced2 = "42") : this(param, optParam: optParam)
     {
     }
-    public TestClass(global::System.Int32 param) : this(param, optParam: default)
+    public TestClass(global::System.Int32 param) : this(param, optParam: default(global::System.Int32))
     {
     }
   }
