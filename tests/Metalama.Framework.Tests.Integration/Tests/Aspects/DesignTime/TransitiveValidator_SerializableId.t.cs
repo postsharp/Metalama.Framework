3d0102a440d222a2878d09fe9794b0741ebafd8e
Daniel Balas <daniel@postsharp.net>
2024-08-05 14:24:21 +0200

    Fix test result.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/DesignTime/TransitiveValidator_SerializableId.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/DesignTime/TransitiveValidator_SerializableId.t.cs
@@ -1,1 +1,1 @@
-// TODO: Replace this file with the correct transformed code. See the test output for the actual transformed code.
+// Warning MY001 on `Foo`: `Reference constraint of type 'OverrideMember' in declaration 'DerivedReferencedClass.Foo' (SyntaxKind=PropertyDeclaration).`
