dea6115c22b988d7d357e11780de309099d5617a
Daniel Balas <daniel@postsharp.net>
2024-09-25 14:40:20 +0200

    Additional test.

--- tests/Metalama.Framework.Tests.UnitTests/CodeModel/PromotedFieldTests.cs
+++ tests/Metalama.Framework.Tests.UnitTests/CodeModel/PromotedFieldTests.cs
@@ -109,4 +109,64 @@ class C<T>
             Assert.True( RefEqualityComparer<IField>.Default.Equals( genericField.ToRef(), genericPromotedField.ToRef().As<IField>() ) );
         }
     }
+
+    [Fact]
+    public void Introduced()
+    {
+        using var testContext = this.CreateTestContext();
+
+        // Create original compilation.
+        const string code = @"
+class C
+{
+}";
+
+        var immutableCompilation1 = testContext.CreateCompilationModel( code );
+
+        var introducedField = new FieldBuilder( null!, immutableCompilation1.Types.Single(), "_f", ObjectReader.Empty );
+        introducedField.Accessibility = Accessibility.Private;
+        introducedField.Type = immutableCompilation1.Factory.GetSpecialType( SpecialType.Int32 );
+
+        // Add the PromotedField to a compilation.
+        var compilation1 = immutableCompilation1.CreateMutableClone();
+        compilation1.AddTransformation( introducedField.ToTransformation() );
+
+        var immutableCompilation2 = compilation1.CreateImmutableClone();
+
+        var field = immutableCompilation2.Types.Single().Fields.Single();
+
+        // Create a PromotedField.
+        var promotedField = PromotedField.Create( testContext.ServiceProvider, field, null!, null! );
+        Assert.Same( promotedField.Definition, promotedField );
+        Assert.Same( promotedField.OverridingProperty, promotedField );
+        Assert.Same( promotedField.OriginalField, promotedField );
+
+        _ = promotedField.PrimarySyntaxTree;
+
+        // Verify that all properties work.
+        var objectReader = new ObjectReaderFactory( testContext.ServiceProvider ).GetReader( promotedField );
+
+        foreach ( var property in objectReader.Keys )
+        {
+            _ = objectReader[property];
+        }
+
+        // Add the PromotedField to a compilation.
+        var compilation2 = immutableCompilation1.CreateMutableClone();
+        compilation2.AddTransformation( promotedField.ToTransformation() );
+
+        // Assertions on declarations.
+        var fieldAfter = field.ForCompilation( compilation2 );
+        Assert.IsType<BuiltField>( fieldAfter );
+        Assert.NotNull( fieldAfter.OverridingProperty );
+        Assert.NotNull( fieldAfter.OverridingProperty.OriginalField );
+        Assert.Same( fieldAfter, fieldAfter.OverridingProperty.OriginalField );
+        Assert.Equal( DeclarationKind.Field, fieldAfter.DeclarationKind );
+        Assert.Equal( DeclarationKind.Property, fieldAfter.OverridingProperty.DeclarationKind );
+
+        // Declaration on references.
+        Assert.Same( fieldAfter, field.ToRef().GetTarget( compilation2 ) );
+        Assert.True( RefEqualityComparer<IField>.Default.Equals( field.ToRef(), promotedField.ToRef().As<IField>() ) );
+        Assert.NotEqual<IRef>( promotedField.ToRef(), promotedField.ToRef().As<IField>() );
+    }
 }
