7d5430743cbec4f2562a9e8ec90db6aa32266068
Antonín Procházka <antonin@postsharp.net>
2024-10-17 17:13:12 +0200

    Investigation.

--- Metalama.Framework.Sdk/Utilities/Roslyn/SyntaxProcessingException.cs
+++ Metalama.Framework.Sdk/Utilities/Roslyn/SyntaxProcessingException.cs
@@ -4,6 +4,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Metalama.Framework.Engine.Utilities.Roslyn;
@@ -26,6 +27,8 @@ internal sealed class SyntaxProcessingException : Exception
 
     private static string GetMessage( SyntaxNode? node, Exception innerException )
     {
+        string message;
+        
         if ( node != null )
         {
             // Get the node text. We need to remove CR and LF otherwise it is not well parsed by MSBuild.
@@ -60,14 +63,21 @@ internal sealed class SyntaxProcessingException : Exception
 
             var location = node.GetLocation();
 
-            return
+            message =
                 $"{innerException.GetType().Name} while processing the {node.Kind()} with code `{nodeText}` at '{nodePath}' in '{location.SourceTree?.FilePath}' ({FormatLinePosition( location.GetMappedLineSpan().StartLinePosition )}-{FormatLinePosition( location.GetMappedLineSpan().EndLinePosition )}): {innerException.Message}";
         }
         else
         {
             // We should never get here because the caller should call ShouldWrapException and not create an exception of our type if the method returns false.  
-            return innerException.Message;
+            message = innerException.Message;
         }
+
+        var directory = Environment.ExpandEnvironmentVariables( @"%TEMP%\Metalama\CrashReports\SyntaxProcessingExceptions" );
+        Directory.CreateDirectory( directory );
+        var path = Path.Combine( directory, $"{Guid.NewGuid()}.txt" );
+        File.WriteAllText( path, message + Environment.NewLine + Environment.NewLine + innerException );
+
+        return message;
     }
 
     private static string FormatLinePosition( in LinePosition position ) => $"{position.Line + 1},{position.Character + 1}";
