85481fc63dd2825847e50a439abc9a49abb2794a
GaelFraiteur <gael@postsharp.net>
2024-10-18 23:54:51 +0200

    Warnings.

--- Metalama.Framework.Engine/CodeModel/Source/FieldHelper.cs
+++ Metalama.Framework.Engine/CodeModel/Source/FieldHelper.cs
@@ -1,4 +1,6 @@
-﻿using Metalama.Framework.Code;
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Framework.Code;
 using Metalama.Framework.Engine.CodeModel.Helpers;
 using Metalama.Framework.Engine.CodeModel.Introductions.BuilderData;
 
