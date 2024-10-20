a0fd7ad9c07486bbad1aed7cc219730622929a9e
GaelFraiteur <gael@postsharp.net>
2024-10-20 13:46:42 +0200

    Accept tests.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Samples/Proxy.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Samples/Proxy.t.cs
@@ -1,10 +1,8 @@
-[assembly: GenerateProxyAspect( typeof(ISomeInterface), "Metalama.Samples.Proxy.Tests", "SomeProxy" )]
-
+// Warning CS0105 on `Metalama.Framework.Code.SyntaxBuilders`: `The using directive for 'Metalama.Framework.Code.SyntaxBuilders' appeared previously in this namespace`
+[assembly: GenerateProxyAspect(typeof(ISomeInterface), "Metalama.Samples.Proxy.Tests", "SomeProxy")]
 public interface ISomeInterface
 {
-    void VoidMethod( int a, string b );
-
-    int NonVoidMethod( int a, string b );
-
-    void VoidNoParamMethod();
+  void VoidMethod(int a, string b);
+  int NonVoidMethod(int a, string b);
+  void VoidNoParamMethod();
 }
