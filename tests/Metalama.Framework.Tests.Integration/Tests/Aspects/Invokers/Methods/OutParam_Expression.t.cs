19a60d3163c70bc442226ea1250aeee04ccdf43b
GaelFraiteur <gael@postsharp.net>
2024-10-20 09:02:45 +0200

    Formatting.

--- tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Invokers/Methods/OutParam_Expression.t.cs
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Invokers/Methods/OutParam_Expression.t.cs
@@ -1,16 +1,13 @@
 public class C
 {
     private object _field = new();
-
     [TheAspect]
     public void Method( out object o )
     {
-        Method_Source( out _field );
-        Method_Source( out o );
-
+    this.Method_Source(out _field);
+    this.Method_Source(out o);
         return;
     }
-
     private void Method_Source( out object o )
     {
         o = new object();
